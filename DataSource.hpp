53d0a223894792dcf9550b4db97368f6f2c4f031
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2013-04-06 01:15:42 +0200

    First version of coarse thread synchronization in DataSource, improves performance.

--- DataSource.hpp
+++ DataSource.hpp
@@ -19,7 +19,7 @@ public:
     
     virtual void wait_for_data(uint64_t min_mcNumber) = 0;
 
-    virtual void update_ack_pointers(uint64_t acked_data, uint64_t acked_mc) = 0;
+    virtual void update_ack_pointers(uint64_t new_acked_data, uint64_t new_acked_mc) = 0;
     
 protected:
     /// Input data buffer.
@@ -41,118 +41,166 @@ public:
         _pd(par->typical_content_size()),
         _rand_content_words(_rng, _pd)
     {
-        _generator_thread = new boost::thread(&DummyFlib::generate_data, this);
+        _producer_thread = new boost::thread(&DummyFlib::produce_data, this);
     };
 
     ~DummyFlib()
     {
-        _is_stopped = true;
-        _generator_thread->join();
-        delete _generator_thread;
+        {
+            boost::unique_lock<boost::mutex> l(_mutex);
+            _is_stopped = true;
+        }
+        _cond_producer.notify_one();
+        _producer_thread->join();
+        delete _producer_thread;
     };
     
     /// Generate FLIB input data.
-    void generate_data()
+    void produce_data()
     {
         bool generate_pattern = par->check_pattern();
 
+        uint64_t written_mc = 0;
+        uint64_t written_data = 0;
+
+        uint64_t last_written_mc = 0;
+        uint64_t last_written_data = 0;
+
+        uint64_t acked_mc = 0;
+        uint64_t acked_data = 0;
+
+        const uint64_t min_avail_mc = _addr_buffer.size() / 4;
+        const uint64_t min_avail_data = _data_buffer.size() / 4;
+
+        const uint64_t min_written_mc = _addr_buffer.size() / 4;
+        const uint64_t min_written_data = _data_buffer.size() / 4;
+
         while (true) {
-            unsigned int content_words = par->typical_content_size();
-            if (par->randomize_sizes())
-                content_words = _rand_content_words();
-
-            uint8_t hdrrev = 0x01;
-            uint8_t sysid = 0x01;
-            uint16_t flags = 0x0000;
-            uint32_t size = (content_words + 2) * 8;
-            uint16_t rsvd = 0x0000;
-            uint64_t time = _mc_written;
-
-            uint64_t hdr0 = (uint64_t) hdrrev << 56 | (uint64_t) sysid << 48
-                            | (uint64_t) flags << 32 | (uint64_t) size;
-            uint64_t hdr1 = (uint64_t) rsvd << 48 | (time & 0xFFFFFFFFFFFF);
-
-            // check for space in data and addr buffers, wait if required
+            // wait until significant space is available
+            last_written_mc = written_mc;
+            last_written_data = written_data;
             {
-                //                boost::unique_lock<boost::mutex> lock(_acked_mutex);
-                while ((_data_written - _acked_data + content_words + 2 > _data_buffer.size())
-                       || (_mc_written - _acked_mc + 1 > _addr_buffer.size())) {
-                    //                    _cond_acked.wait(lock);
-                    if (_is_stopped) return;
+                boost::unique_lock<boost::mutex> l(_mutex);
+                _written_mc = written_mc;
+                _written_data = written_data;
+                if (_is_stopped)
+                    return;
+                while ((written_data - _acked_data + min_avail_data > _data_buffer.size())
+                       || (written_mc - _acked_mc + min_avail_mc > _addr_buffer.size())) {
+                    _cond_producer.wait(l);
+                    if (_is_stopped)
+                        return;
+                }
+                acked_mc = _acked_mc;
+                acked_data = _acked_data;
+            }
+
+            while (true) {
+                unsigned int content_words = par->typical_content_size();
+                if (par->randomize_sizes())
+                    content_words = _rand_content_words();
+
+                // check for space in data and addr buffers
+                if ((written_data - acked_data + content_words + 2 > _data_buffer.size())
+                    || (written_mc - acked_mc + 1 > _addr_buffer.size()))
+                    break;
+
+                uint8_t hdrrev = 0x01;
+                uint8_t sysid = 0x01;
+                uint16_t flags = 0x0000;
+                uint32_t size = (content_words + 2) * 8;
+                uint16_t rsvd = 0x0000;
+                uint64_t time = written_mc;
+
+                uint64_t hdr0 = (uint64_t) hdrrev << 56 | (uint64_t) sysid << 48
+                    | (uint64_t) flags << 32 | (uint64_t) size;
+                uint64_t hdr1 = (uint64_t) rsvd << 48 | (time & 0xFFFFFFFFFFFF);
+
+                // write to data buffer
+                uint64_t start_addr = written_data;
+                _data_buffer.at(written_data++) = hdr0;
+                _data_buffer.at(written_data++) = hdr1;
+
+                if (generate_pattern) {
+                    for (uint64_t i = 0; i < content_words; i++) {
+                        _data_buffer.at(written_data++) = ((uint64_t) par->node_index() << 48) | i;
+                    }
+                } else {
+                    written_data += content_words;
+                }
+
+                // write to addr buffer
+                _addr_buffer.at(written_mc++) = start_addr;
+
+                if (written_mc >= last_written_mc + min_written_mc
+                    || written_data >= last_written_data + min_written_data) {
+                    last_written_mc = written_mc;
+                    last_written_data = written_data;
+                    {
+                        boost::unique_lock<boost::mutex> l(_mutex);
+                        _written_mc = written_mc;
+                        _written_data = written_data;
+                    }
+                    _cond_consumer.notify_one();
                 }
             }
-
-            // write to data buffer
-            uint64_t start_addr = _data_written;
-            _data_buffer.at(_data_written++) = hdr0;
-            _data_buffer.at(_data_written++) = hdr1;
-
-            if (generate_pattern) {
-                for (uint64_t i = 0; i < content_words; i++) {
-                    _data_buffer.at(_data_written++) = ((uint64_t) par->node_index() << 48) | i;
-                }
-            }
-
-            // write to addr buffer
-            _addr_buffer.at(_mc_written) = start_addr;
-            {
-                //                boost::unique_lock<boost::mutex> lock(_written_mutex);
-                _mc_written++;
-            }
-            //_cond_written.notify_all();
-            if (_is_stopped) return;
         }
     }
 
-
-    //    to do: this is a bad idea (performance). minimize inter-thread comm!
-
     virtual void wait_for_data(uint64_t min_mc_number)
     {
-        static uint64_t local_mc_written = 0;
+        static uint64_t written_mc = 0;
 
-        if (min_mc_number > local_mc_written) {
-            //            boost::unique_lock<boost::mutex> lock(_written_mutex);
-            while (min_mc_number > _mc_written) {
-                //                _cond_written.wait(lock);
-                boost::this_thread::yield();
+        if (min_mc_number > written_mc) {
+            boost::unique_lock<boost::mutex> l(_mutex);
+            while (min_mc_number > _written_mc) {
+                _cond_consumer.wait(l);
             }
-            local_mc_written = _mc_written;
+            written_mc = _written_mc;
         }
     };
 
-    virtual void update_ack_pointers(uint64_t acked_data, uint64_t acked_mc)
+    virtual void update_ack_pointers(uint64_t new_acked_data, uint64_t new_acked_mc)
     {
-        {
-            //            boost::unique_lock<boost::mutex> lock(_acked_mutex);
-            _acked_data = acked_data;
-            _acked_mc = acked_mc;
+        static uint64_t acked_mc = 0;
+        static uint64_t acked_data = 0;
+
+        const uint64_t min_acked_mc = _addr_buffer.size() / 4;
+        const uint64_t min_acked_data = _data_buffer.size() / 4;
+
+        if (new_acked_data >= acked_data + min_acked_data
+            || new_acked_mc >= acked_mc + min_acked_mc) {
+            acked_data = new_acked_data;
+            acked_mc = new_acked_mc;
+            {
+                boost::unique_lock<boost::mutex> l(_mutex);
+                _acked_data = acked_data;
+                _acked_mc = acked_mc;
+            }
+            _cond_producer.notify_one();
         }
-        //        _cond_acked.notify_all();
     };
 
 private:
     std::atomic<bool> _is_stopped{false};
 
-    boost::thread* _generator_thread;
+    boost::thread* _producer_thread;
 
-    boost::mutex _acked_mutex;
-    boost::condition_variable _cond_acked;
-
-    boost::mutex _written_mutex;
-    boost::condition_variable _cond_written;
+    boost::mutex _mutex;
+    boost::condition_variable _cond_producer;
+    boost::condition_variable _cond_consumer;
 
     /// Number of acknowledged data words. Updated by input node.
-    std::atomic<uint64_t> _acked_data{0};
+    uint64_t _acked_data{0};
     
     /// Number of acknowledged MCs. Updated by input node.
-    std::atomic<uint64_t> _acked_mc{0};
+    uint64_t _acked_mc{0};
     
     /// FLIB-internal number of written data words. 
-    uint64_t _data_written = 0;
+    uint64_t _written_data{0};
 
     /// FLIB-internal number of written MCs. 
-    std::atomic<uint64_t> _mc_written{0};
+    uint64_t _written_mc{0};
 
     /// A pseudo-random number generator.
     boost::mt19937 _rng;
