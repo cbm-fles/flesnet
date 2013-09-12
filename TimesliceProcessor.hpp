51d0f09221085cef7affc527fa7307112b593ac9
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2013-09-12 18:16:36 +0200

    Simplify TimesliceProcessor assuming no buffer wrap.

--- TimesliceProcessor.hpp
+++ TimesliceProcessor.hpp
@@ -36,42 +36,34 @@ public:
         uint64_t ts_pos = wi.ts_pos;
         uint64_t ts_num = _cb.get_desc(0, ts_pos).ts_num;
         uint64_t ts_size = par->timeslice_size() + par->overlap_size();
+
         if (out.beDebug()) {
             out.debug() << "processor thread " << _index << " working on timeslice " << ts_num;
         }
 
-        const uint64_t data_size = (1 << par->cn_data_buffer_size_exp());
-        const uint64_t data_mask = data_size - 1;
-
         for (size_t in = 0; in < _cb.size(); in++) {
-            assert(_cb.get_desc(in, ts_pos).ts_num == ts_num);
+            TimesliceComponentDescriptor& desc = _cb.get_desc(in, ts_pos);
+            assert(desc.ts_num == ts_num);
 
-            //uint64_t size = _cb.get_desc(in, ts_pos).size;
-            uint64_t offset = _cb.get_desc(in, ts_pos).offset;
+            MicrosliceDescriptor* mc_desc =
+                &reinterpret_cast<MicrosliceDescriptor&>(_cb.get_data(in, desc.offset));
 
-            //const RingBuffer<>& data = _cb.data(in);
+            uint8_t* mc_data =
+                &_cb.get_data(in, desc.offset) + ts_size * sizeof(MicrosliceDescriptor);
 
-            uint64_t mc_offset = ((MicrosliceDescriptor&) _cb.get_data(in, offset)).offset;
             for (size_t mc = 0; mc < ts_size; mc++) {
-                MicrosliceDescriptor& mc_desc =
-                    ((MicrosliceDescriptor&) _cb.get_data
-                     (in, offset +  mc * sizeof(MicrosliceDescriptor)));
-                uint64_t this_offset = mc_desc.offset - mc_offset + offset
-                    + ts_size * sizeof(MicrosliceDescriptor);
-                assert(mc_desc.idx == ts_num * par->timeslice_size() + mc);
-
+                assert(mc_desc[mc].idx == ts_num * par->timeslice_size() + mc);
+                uint64_t* this_mc_data =
+                    reinterpret_cast<uint64_t*>(mc_data + mc_desc[mc].offset - mc_desc[0].offset);
                 if (check_pattern) {
                     uint32_t crc = 0x00000000;
-                    if ((this_offset & data_mask) + mc_desc.size > data_size) {
-                        out.warn() << "timeslice " << ts_num << " crosses buffer boundary";
-                    }
-                    for (size_t pos = 0; pos < mc_desc.size; pos += sizeof(uint64_t)) {
-                        uint64_t data_word = (uint64_t &) _cb.get_data(in, this_offset + pos);
+                    for (size_t pos = 0; pos < mc_desc[mc].size; pos += sizeof(uint64_t)) {
+                        uint64_t data_word = this_mc_data[pos / sizeof(uint64_t)];
                         crc ^= (data_word & 0xffffffff) ^ (data_word >> 32);
                         uint64_t expected = ((uint64_t) in << 48) | pos;
                         assert(data_word == expected);
                     }
-                    assert(crc == mc_desc.crc);
+                    assert(crc == mc_desc[mc].crc);
                 }
             }
         }
