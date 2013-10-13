879bcf8a7bf5bb12505cc0753fed396915fb729e
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2013-10-13 14:42:24 +0200

    Continue flib interface implementation.

--- lib/fles_core/FlibHardwareChannel.cpp
+++ lib/fles_core/FlibHardwareChannel.cpp
@@ -14,6 +14,7 @@ FlibHardwareChannel::FlibHardwareChannel(std::size_t data_buffer_size_exp,
 {
     _flib_link->init_dma(
         flib::create_only, data_buffer_size_exp, desc_buffer_size_exp);
+    // TODO: reduce desc_buffer_size_exp (sizeof MicrosliceDescriptor)?
 
     uint8_t* data_buffer = reinterpret_cast
         <uint8_t*>(_flib_link->ebuf()->getMem());
@@ -26,21 +27,19 @@ FlibHardwareChannel::FlibHardwareChannel(std::size_t data_buffer_size_exp,
         new RingBufferView
         <MicrosliceDescriptor>(desc_buffer, desc_buffer_size_exp));
 
-    // set the aproriate header config
     flib::hdr_config config{static_cast<uint16_t>(0xE000 + input_index), 0xBC,
                             0xFD};
     _flib_link->set_hdr_config(&config);
 
-    // reset pending mc from potential previous run
     _flib_link->rst_pending_mc();
 
-    // set data source and enable
     _flib_link->set_data_rx_sel(flib::flib_link::pgen);
     _flib_link->enable_cbmnet_packer(true);
 }
 
 FlibHardwareChannel::~FlibHardwareChannel()
 {
+    _flib_link->rst_pending_mc();
 }
 
 uint64_t FlibHardwareChannel::wait_for_data(uint64_t min_mc_number)
@@ -51,4 +50,8 @@ uint64_t FlibHardwareChannel::wait_for_data(uint64_t min_mc_number)
 void FlibHardwareChannel::update_ack_pointers(uint64_t new_acked_data,
                                               uint64_t new_acked_mc)
 {
+    _flib_link->get_ch()->setOffsets(
+        new_acked_data & _data_buffer_view->size_mask(),
+        (new_acked_mc & _desc_buffer_view->size_mask())
+        * sizeof(MicrosliceDescriptor));
 }
