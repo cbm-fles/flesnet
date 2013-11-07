ae02d4d24d65e6242bf818b345dd8dc4b1869987
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2013-11-07 18:04:36 +0100

    Fix bug in human_readable_byte_count().

--- lib/fles_core/Timeslice.hpp
+++ lib/fles_core/Timeslice.hpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <cmath>
 #include <rdma/rdma_cma.h>
+#include <boost/lexical_cast.hpp>
 
 #pragma pack(1)
 
@@ -176,12 +177,13 @@ inline std::ostream& operator<<(std::ostream& s, ibv_send_flags v)
         return s << str;
 }
 
-inline std::string human_readable_byte_count(uint64_t bytes, bool use_si = false)
+inline std::string human_readable_byte_count(uint64_t bytes,
+                                             bool use_si = false)
 {
     uint64_t unit = use_si ? 1000 : 1024;
 
     if (bytes < unit)
-        return bytes + " B";
+        return boost::lexical_cast<std::string>(bytes) + " B";
 
     uint32_t exponent = static_cast<uint64_t>(std::log(bytes) / std::log(unit));
 
