4250aae5d6648832b1af9cda57bfd1309498fdbf
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2013-11-15 14:44:29 +0100

    Add members to TimesliceArchiveDescriptor.

--- TimesliceArchiveDescriptor.hpp
+++ TimesliceArchiveDescriptor.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <chrono>
+#include <string>
 #include <boost/serialization/access.hpp>
 
 namespace fles
@@ -16,11 +17,28 @@ public:
         : _time_created(std::chrono::system_clock::to_time_t(
               std::chrono::system_clock::now()))
     {
+        _hostname = std::string("---");
+        _username = current_username();
+        // TODO: add hostname, username
     }
 
     /// Retrieve the time of creation of the archive.
-    std::time_t time_created() const;
-    // TODO: add hostname, username etc.
+    std::time_t time_created() const
+    {
+        return _time_created;
+    }
+
+    /// Retrieve the hostname of the machine creating the archive.
+    std::string hostname() const
+    {
+        return _hostname;
+    }
+
+    /// Retrieve the hostname of the machine creating the archive.
+    std::string username() const
+    {
+        return _username;
+    }
 
 private:
     friend class boost::serialization::access;
@@ -28,9 +46,15 @@ private:
     void serialize(Archive& ar, const unsigned int /* version */)
     {
         ar& _time_created;
+        ar& _hostname;
+        ar& _username;
     }
 
+    std::string current_username() const;
+
     std::time_t _time_created;
+    std::string _hostname;
+    std::string _username;
 };
 
 } // namespace fles {
