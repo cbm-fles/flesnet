47abd3be696c72e3d3b70307d6aafc105c89298b
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2014-03-18 17:02:31 +0100

    Added basic multi link support to controls software

--- app/flib_ctrl/flib_ctrl.cpp
+++ app/flib_ctrl/flib_ctrl.cpp
@@ -1,6 +1,7 @@
 #include <csignal>
 
 #include <zmq.hpp>
+#include <boost/lexical_cast.hpp>
 #include <control/libserver/ControlServer.hpp>
 
 #include "global.hpp"
@@ -38,21 +39,45 @@ int main(int argc, const char* argv[])
   flib::flib_device flib(0);
   std::vector<flib::flib_link*> links = flib.get_links();
 
-  // configure FLIB link
-  links.at(0)->set_data_rx_sel(flib::flib_link::pgen);
-  //links.at(0)->set_data_rx_sel(flib::flib_link::link);
+  // FLIB global configuration
+  flib.set_mc_time(1000);
 
-  // create device control server, initialize and start server thread
-  flib_server flibserver(zmq_context, "CbmNet::Driver0", flib, *links.at(0));
-  flibserver.Bind();
-  flibserver.Start();
-  
-  // create control server and start
-  CbmNet::ControlServer cntlserv(zmq_context, "CbmNet::Driver0");
-  //cntlserv.SetDebugFlags(CbmNet::ControlServer::kDbgDumpRpc);
-  cntlserv.Bind();
-  cntlserv.ConnectDriver();
-  cntlserv.Start();
+  // FLIB per link configuration
+  std::vector<std::unique_ptr<flib_server>> flibserver;
+  std::vector<std::unique_ptr<CbmNet::ControlServer>> ctrlserver;
+
+  for (size_t i = 0; i < flib.get_num_links(); ++i) {
+    out.debug() << "initializing link " << i;
+
+    // enable data source
+    bool use_sp = false; 
+    if (use_sp == true) {
+      links.at(i)->set_data_rx_sel(flib::flib_link::link);
+      links.at(i)->prepare_dlm(0x8, true); // enable DAQ
+      links.at(i)->send_dlm();
+    } 
+    else {
+      links.at(i)->set_data_rx_sel(flib::flib_link::pgen);
+    }
+
+    // create device control server, initialize and start server thread
+    flibserver.push_back(std::unique_ptr<flib_server>(
+        new flib_server(zmq_context, "CbmNet::Driver" + 
+                        boost::lexical_cast<std::string>(i), 
+                        flib, *links.at(i))));
+    flibserver.at(i)->Bind();
+    flibserver.at(i)->Start();
+
+    // create control server and start
+    ctrlserver.push_back(std::unique_ptr<CbmNet::ControlServer>(
+         new CbmNet::ControlServer(zmq_context, "CbmNet::Driver" 
+                                   + boost::lexical_cast<std::string>(i))));
+    //ctrlserver.at(i)->SetDebugFlags(CbmNet::ControlServer::kDbgDumpRpc);    
+    ctrlserver.at(i)->Bind("tcp://*:" + boost::lexical_cast<std::string>(9750 + i));
+    ctrlserver.at(i)->ConnectDriver();
+    ctrlserver.at(i)->Start();
+
+  }
 
   // main loop
   while(s_interrupted==0) {
