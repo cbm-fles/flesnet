14dc023d7d77079ba0af39a53e32720f0d7aae97
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-07-03 20:28:29 +0200

    - removed more obsolete stuff

--- lib/flib/include/flib/rorcfs_bar.hh
+++ lib/flib/include/flib/rorcfs_bar.hh
@@ -1,24 +1,10 @@
 /**
- * @file rorcfs_bar.hh
- * @author Heiko Engel <hengel@cern.ch>
+ * @file
+ * @author Dominic Eschweiler <dominic.eschweiler@cern.ch>
  * @version 0.1
- * @date 2011-08-16
+ * @date 2014-07-03
  *
- * @section LICENSE
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @section DESCRIPTION
- *
- * The rorcfs_bar class represents a Base Address Register (BAR) file
+ * The bar class represents a Base Address Register (BAR) file
  * mapping of the RORCs PCIe address space
  */
 
@@ -30,60 +16,35 @@
 #define BAR_ERROR_CONSTRUCTOR_FAILED 0
 
 #include "rorcfs_device.hh"
-#include <sys/stat.h>
 #include <pthread.h>
 
 /**
- * @class rorcfs_bar
+ * @class
  * @brief Represents a Base Address Register (BAR) file
  * mapping of the RORCs PCIe address space
- *
- * Create a new rorcfs_bar object after initializing your
- * rorcfs_device instance. <br>Once your rorcfs_bar instance is
- * initialized (with init()) you can use get() and set() to
- * read from and/or write to the device.
  */
 class rorcfs_bar
 {
 public:
-  /**
-   * Constructor that sets fname accordingly. No mapping is
-   * performed at this point.
-   * @param dev parent rorcfs_device
-   * @param n number of BAR to be mapped [0-6]
-   **/
-  rorcfs_bar(device* dev, uint8_t n);
+    /**
+     * Constructor that sets fname accordingly. No mapping is
+     * performed at this point.
+     * @param dev parent rorcfs_device
+     * @param n number of BAR to be mapped [0-6]
+    **/
+     rorcfs_bar(device* dev, uint8_t number);
+    ~rorcfs_bar();
 
-  /**
-   * Deconstructor: free fname, unmap BAR, close file
-   **/
-  ~rorcfs_bar();
-
-//  /**
-//   * initialize BAR mapping: open sysfs file, get file stats,
-//   * mmap file. This has to be done before using any other
-//   * member funtion. This function will fail if the requested
-//   * BAR does not exist.
-//   * @return 0 on sucess, -1 on errors
-//   **/
-//  int init();
 
     void* get_mem_ptr(){return static_cast<void*>(m_bar);};
 
     /**
-     * get size of mapped BAR. This value is only valid after init()
+     * Get size of mapped BAR. This value is only valid after init()
      * @return size of mapped BAR in (unsigned long) bytes
      **/
     size_t get_size(){return(m_size);};
 
 protected:
-    int handle;
-    char* fname;
-    struct stat barstat;
-    unsigned int* bar;
-    pthread_mutex_t mtx;
-
-    /** NEW */
     device          *m_parent_dev;
     PciDevice       *m_pda_pci_device;
     pthread_mutex_t  m_mtx;
--- lib/flib/src/rorcfs_bar.cpp
+++ lib/flib/src/rorcfs_bar.cpp
@@ -1,34 +1,6 @@
-/**
- * @file rorcfs_bar.cpp
- * @author Heiko Engel <hengel@cern.ch>
- * @version 0.1
- * @date 2011-08-16
- * 
- * @section LICENSE
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- */
-
-#include <iostream>
-#include <sys/stat.h>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <assert.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <stdint.h>
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
 
 #include "../include/flib/rorcfs.h"
 #include "../include/flib/rorcfs_bar.hh"
@@ -51,15 +23,4 @@ rorcfs_bar::rorcfs_bar
     { throw BAR_ERROR_CONSTRUCTOR_FAILED; }
 
     pthread_mutex_init(&m_mtx, NULL);
-}
-
-rorcfs_bar::~rorcfs_bar()
-{
-//	pthread_mutex_destroy(&mtx);
-//	if (fname)
-//		free(fname);
-//	if (bar)
-//		munmap( bar, barstat.st_size);
-//	if (handle)
-//		close(handle);
 }
