0d591e00ea6a27bbb208122e1397f6fac9ea33c9
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-04 10:12:16 +0200

    give a better name to the vector containing DTMs

--- lib/fles_ipc/MicrosliceContents.cpp
+++ lib/fles_ipc/MicrosliceContents.cpp
@@ -7,19 +7,19 @@ namespace fles {
 // extract pointers to DTMs from a microslice
 static std::vector<DTM> get_dtms(const uint16_t *data, size_t size)
 {
-    std::vector<DTM> v;
+    std::vector<DTM> dtms;
     const uint16_t *end = data + size;
     while (data < end) {
         size_t i = 0;
         size_t len = (data[i++] & 0xFF) + 1;
         if (len > 1) {
-            v.push_back(DTM {data+i, len});
+            dtms.push_back(DTM {data+i, len});
             i += len;
         }
         i += (~i & 3) + 1; // skip padding (i -> k*4)
         data += i;
     }
-    return v;
+    return dtms;
 }
 
 MicrosliceContents::MicrosliceContents(const uint16_t *data, size_t size)
