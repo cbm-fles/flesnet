f96b54531a75bebbd5183638096e6225ea4059ff
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2014-07-08 10:44:24 +0200

    Bugfix for double register access problem in conjunction with SSE.

--- lib/flib/include/flib/register_file_bar.hpp
+++ lib/flib/include/flib/register_file_bar.hpp
@@ -41,7 +41,13 @@ public:
   int set_mem(sys_bus_addr addr, const void *source, size_t dwords) override {
     sys_bus_addr sys_addr = _base_addr + addr;
     if( ((sys_addr + dwords) << 2) < _bar_size) {      
-      memcpy(reinterpret_cast<uint8_t*>(_bar) + (sys_addr<<2), source, dwords<<2);
+      // memcpy(reinterpret_cast<uint8_t*>(_bar) + (sys_addr<<2), source, dwords<<2);
+      // TODO: don't use memcpy to prevent double writes in
+      // conjuction with SSE/SSE2 register usage.
+      // See Intel Software Developer’s Manual Vol. 3A 11-8
+      for (size_t i = 0; i < dwords; i++) {
+       _bar[sys_addr + i] =  *(static_cast<const uint32_t*>(source) + i);
+      }
       return msync( (reinterpret_cast<uint8_t*>(_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
     } else {
       return -1;
