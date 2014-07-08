2695f7e8178335cf8b8df86c78df3c8caf6540cb
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-07-08 14:34:11 +0200

    - moved stuff to cpp

--- lib/flib/include/flib/register_file_bar.hpp
+++ lib/flib/include/flib/register_file_bar.hpp
@@ -20,37 +20,15 @@ namespace flib
         register_file_bar(pci_bar* bar, sys_bus_addr base_addr)
         : m_base_addr(base_addr)
         {
-            m_bar = static_cast<uint32_t*>(bar->get_mem_ptr());
+            m_bar      = static_cast<uint32_t*>(bar->get_mem_ptr());
             m_bar_size = bar->get_size();
         }
 
         int
-        get_mem(sys_bus_addr addr, void *dest, size_t dwords) override
-        {
-            /** sys_bus hw only supports single 32 bit reads */
-            sys_bus_addr sys_addr = m_base_addr + addr;
-            if( ((sys_addr + dwords) << 2) < m_bar_size)
-            {
-                for (size_t i = 0; i < dwords; i++)
-                { *(static_cast<uint32_t*>(dest) + i) = m_bar[sys_addr + i]; }
-                return 0;
-            }
-            else
-            { return -1; }
-        }
+        get_mem(sys_bus_addr addr, void *dest, size_t dwords) override;
 
         int
-        set_mem(sys_bus_addr addr, const void *source, size_t dwords) override
-        {
-            sys_bus_addr sys_addr = m_base_addr + addr;
-            if( ((sys_addr + dwords) << 2) < m_bar_size)
-            {
-                memcpy(reinterpret_cast<uint8_t*>(m_bar) + (sys_addr<<2), source, dwords<<2);
-                return msync( (reinterpret_cast<uint8_t*>(m_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
-            }
-            else
-            { return -1; }
-        }
+        set_mem(sys_bus_addr addr, const void *source, size_t dwords) override;
 
     protected:
         uint32_t*    m_bar; // 32 bit addressing
---
+++ lib/flib/src/register_file_bar.cpp
@@ -0,0 +1,48 @@
+
+#include <flib/register_file_bar.hpp>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+
+namespace flib
+{
+    int
+    register_file_bar::get_mem
+    (
+        sys_bus_addr  addr,
+        void         *dest,
+        size_t        dwords
+    )
+    {
+        /** sys_bus hw only supports single 32 bit reads */
+        sys_bus_addr sys_addr = m_base_addr + addr;
+        if( ((sys_addr + dwords) << 2) < m_bar_size)
+        {
+            for (size_t i = 0; i < dwords; i++)
+            { *(static_cast<uint32_t*>(dest) + i) = m_bar[sys_addr + i]; }
+            return 0;
+        }
+        else
+        { return -1; }
+    }
+
+
+    int
+    register_file_bar::set_mem
+    (
+        sys_bus_addr addr,
+        const void *source,
+        size_t dwords
+    )
+    {
+        sys_bus_addr sys_addr = m_base_addr + addr;
+        if( ((sys_addr + dwords) << 2) < m_bar_size)
+        {
+            memcpy(reinterpret_cast<uint8_t*>(m_bar) + (sys_addr<<2), source, dwords<<2);
+            return msync( (reinterpret_cast<uint8_t*>(m_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
+        }
+        else
+        { return -1; }
+    }
+}
