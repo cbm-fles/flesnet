4d1f9ff9aa5703bb832bf172bfc023e2a7280d39
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-07-08 14:18:35 +0200

    - some renaming

--- lib/flib/include/flib/register_file_bar.hpp
+++ lib/flib/include/flib/register_file_bar.hpp
@@ -18,21 +18,21 @@ namespace flib
     public:
 
         register_file_bar(pci_bar* bar, sys_bus_addr base_addr)
-        : _base_addr(base_addr)
+        : m_base_addr(base_addr)
         {
-            _bar = static_cast<uint32_t*>(bar->get_mem_ptr());
-            _bar_size = bar->get_size();
+            m_bar = static_cast<uint32_t*>(bar->get_mem_ptr());
+            m_bar_size = bar->get_size();
         }
 
         int
         get_mem(sys_bus_addr addr, void *dest, size_t dwords) override
         {
             /** sys_bus hw only supports single 32 bit reads */
-            sys_bus_addr sys_addr = _base_addr + addr;
-            if( ((sys_addr + dwords) << 2) < _bar_size)
+            sys_bus_addr sys_addr = m_base_addr + addr;
+            if( ((sys_addr + dwords) << 2) < m_bar_size)
             {
                 for (size_t i = 0; i < dwords; i++)
-                { *(static_cast<uint32_t*>(dest) + i) = _bar[sys_addr + i]; }
+                { *(static_cast<uint32_t*>(dest) + i) = m_bar[sys_addr + i]; }
                 return 0;
             }
             else
@@ -42,20 +42,20 @@ namespace flib
         int
         set_mem(sys_bus_addr addr, const void *source, size_t dwords) override
         {
-            sys_bus_addr sys_addr = _base_addr + addr;
-            if( ((sys_addr + dwords) << 2) < _bar_size)
+            sys_bus_addr sys_addr = m_base_addr + addr;
+            if( ((sys_addr + dwords) << 2) < m_bar_size)
             {
-                memcpy(reinterpret_cast<uint8_t*>(_bar) + (sys_addr<<2), source, dwords<<2);
-                return msync( (reinterpret_cast<uint8_t*>(_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
+                memcpy(reinterpret_cast<uint8_t*>(m_bar) + (sys_addr<<2), source, dwords<<2);
+                return msync( (reinterpret_cast<uint8_t*>(m_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
             }
             else
             { return -1; }
         }
 
     protected:
-        uint32_t*    _bar; // 32 bit addressing
-        size_t       _bar_size;
-        sys_bus_addr _base_addr;
+        uint32_t*    m_bar; // 32 bit addressing
+        size_t       m_bar_size;
+        sys_bus_addr m_base_addr;
     };
 }
 #endif /** REGISTER_FILE_BAR_HPP */
