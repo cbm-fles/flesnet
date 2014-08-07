9a730f43a4bc12a4b77d77a0d42c990ff73bfb3f
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-08-07 14:48:46 +0200

    - fixed merge

--- lib/flib/src/register_file_bar.cpp
+++ lib/flib/src/register_file_bar.cpp
@@ -14,12 +14,15 @@ register_file_bar::register_file_bar(pci_bar *bar, sys_bus_addr base_addr)
   m_bar_size = bar->get_size();
 }
 
-int register_file_bar::get_mem(sys_bus_addr addr, void *dest, size_t dwords) {
-  /** sys_bus hw only supports single 32 bit reads */
+
+
+__attribute__((__target__("no-sse")))
+int register_file_bar::get_mem(sys_bus_addr addr, void *dest, size_t dwords){
+  // sys_bus hw only supports single 32 bit reads
   sys_bus_addr sys_addr = m_base_addr + addr;
-  if (((sys_addr + dwords) << 2) < m_bar_size) {
+  if( ((sys_addr + dwords) << 2) < m_bar_size) {
     for (size_t i = 0; i < dwords; i++) {
-      *(static_cast<uint32_t *>(dest) + i) = m_bar[sys_addr + i];
+      *(static_cast<uint32_t*>(dest) + i) = m_bar[sys_addr + i];
     }
     return 0;
   } else {
@@ -27,25 +30,24 @@ int register_file_bar::get_mem(sys_bus_addr addr, void *dest, size_t dwords) {
   }
 }
 
-int register_file_bar::set_mem(sys_bus_addr addr, const void *source,
-                               size_t dwords) {
+
+
+__attribute__((__target__("no-sse")))
+int register_file_bar::set_mem(sys_bus_addr addr, const void *source, size_t dwords){
   sys_bus_addr sys_addr = m_base_addr + addr;
-  if (((sys_addr + dwords) << 2) < m_bar_size) {
-    //memcpy(reinterpret_cast<uint8_t*>(m_bar) + (sys_addr<<2), source,
-    //dwords<<2);
-    // memcpy(reinterpret_cast<uint8_t*>(_bar) + (sys_addr<<2), source,
-    // dwords<<2);
+  if( ((sys_addr + dwords) << 2) < m_bar_size) {
+    // memcpy(reinterpret_cast<uint8_t*>(_bar) + (sys_addr<<2), source, dwords<<2);
     // TODO: don't use memcpy to prevent double writes in
     // conjuction with SSE/SSE2 register usage.
     // See Intel Software Developer’s Manual Vol. 3A 11-8
     for (size_t i = 0; i < dwords; i++) {
-      m_bar[sys_addr + i] = *(static_cast<const uint32_t *>(source) + i);
+     m_bar[sys_addr + i] =  *(static_cast<const uint32_t*>(source) + i);
     }
-    return msync(
-        (reinterpret_cast<uint8_t *>(m_bar) + ((sys_addr << 2) & PAGE_MASK)),
-        PAGE_SIZE, MS_SYNC);
+    return msync( (reinterpret_cast<uint8_t*>(m_bar) + ((sys_addr<<2) & PAGE_MASK)), PAGE_SIZE, MS_SYNC);
   } else {
     return -1;
   }
 }
+
+
 }
