8f73115616deaba73ee53896b0e710754d9034a6
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-08-28 13:25:42 +0200

    added initial version of MicrosliceContainer: combines microslice descriptor and payload to offer a cleaner interface to higher-level applications

---
+++ MicrosliceContainer.hpp
@@ -0,0 +1,29 @@
+#include "MicrosliceDescriptor.hpp"
+#include <vector>
+
+namespace fles {
+
+/**
+ * Combines microslice metadata plus payload.
+ */
+struct MicrosliceContainer {
+    MicrosliceDescriptor desc;
+    std::vector<uint8_t> content;
+
+    MicrosliceContainer(MicrosliceDescriptor d, std::vector<uint8_t> c)
+    : desc (d), content (std::move(c))
+    {
+        desc.size = content.size();
+    };
+    /**<
+     * no vector is copied if an rvalue is passed for c,
+     * like in
+     *     MicrosliceContainer {..., std::move(some_vector)}
+     * or
+     *     MicrosliceContainer {..., {1, 2, 3, 4, 5}}
+     * or
+     *     MicrosliceContainer {..., make_vector()}
+     */
+};
+
+}
