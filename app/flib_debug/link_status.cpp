8c2fc713409a47e37eb818763fb2185fa19d3da0
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2014-09-01 15:49:46 +0200

    Extended link_status to all links

--- app/flib_debug/link_status.cpp
+++ app/flib_debug/link_status.cpp
@@ -18,6 +18,18 @@ using namespace flib;
 
 flib_device* MyFlib = NULL;
 
+std::ostream& operator<<(std::ostream& os, flib_link::data_sel_t sel)
+{
+  switch(sel) {
+  case flib_link::rx_disable : os << "disable"; break;
+  case flib_link::rx_emu : os <<     "    emu"; break;
+  case flib_link::rx_link : os <<    "   link"; break;
+  case flib_link::rx_pgen : os <<    "   pgen"; break;
+  default : os.setstate(std::ios_base::failbit);
+  }
+  return os;
+}
+
 int main(int argc, char *argv[])
 {
   MyFlib = new flib_device(0);
@@ -25,17 +37,20 @@ int main(int argc, char *argv[])
   std::cout << MyFlib->print_build_info() << std::endl;
   std::cout << MyFlib->print_devinfo() << std::endl;
 
-  //MyFlib->link(0).set_data_sel(flib_link::pgen);
-    
-  flib::flib_link::link_status_t status = MyFlib->link(0).link_status();
-  
-  std::stringstream ss;
-  ss << "link status" << "\n"
-     << "link_active " << status.link_active << "\n"
-     << "data_rx_stop " << status.data_rx_stop << "\n"
-     << "ctrl_rx_stop " << status.ctrl_rx_stop << "\n"
-     << "ctrl_tx_stop " << status.ctrl_tx_stop << "\n";
+  size_t num_links = MyFlib->number_of_links();
+  std::vector<flib_link*>links = MyFlib->links();
 
+  std::stringstream ss;
+  ss << "\nlink  data_sel  link_active  data_rx_stop  ctrl_rx_stop  ctrl_tx_stop \n";
+  for( size_t i = 0; i < num_links; ++i) {
+     ss << "   " << i
+        << "   "<< links.at(i)->data_sel()
+        << "            "<< links.at(i)->link_status().link_active
+        << "             "<< links.at(i)->link_status().data_rx_stop
+        << "             "<< links.at(i)->link_status().ctrl_rx_stop
+        << "             "<< links.at(i)->link_status().ctrl_tx_stop
+        << "\n";
+  }
   std::cout << ss.str();
 
   if (MyFlib)
