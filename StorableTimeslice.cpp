768e534be6e65d5968a96f7dc5fae1e205995628
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-02 13:20:48 +0200

    copy-construct _data and _desc members of StorableTimeslice instead of copying the contents "by hand"

--- StorableTimeslice.cpp
+++ StorableTimeslice.cpp
@@ -6,18 +6,9 @@ namespace fles
 {
 
 StorableTimeslice::StorableTimeslice(const Timeslice& ts)
-    : _data(ts._timeslice_descriptor.num_components),
-      _desc(ts._timeslice_descriptor.num_components)
+    : _data(ts._data),
+      _desc(ts._desc)
 {
-    _timeslice_descriptor = ts._timeslice_descriptor;
-    for (std::size_t component = 0;
-         component < ts._timeslice_descriptor.num_components; ++component) {
-        uint64_t size = ts._desc_ptr[component]->size;
-        _data[component].resize(size);
-        std::copy_n(ts._data_ptr[component], size, _data[component].begin());
-        _desc[component] = *ts._desc_ptr[component];
-    }
-
     init_pointers();
 }
 
