da30ee8e4b97c535d986c5697e1f4ebf0a78171d
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-09 17:35:41 +0200

    renamed MicrosliceContainer -> StorableMicroslice to be consistent with Timeslices

--- StorableMicroslice.hpp
+++ StorableMicroslice.hpp
@@ -8,29 +8,29 @@ namespace fles {
 /**
  * Store microslice metadata and content in a single object.
  */
-struct MicrosliceContainer {
-    MicrosliceContainer(const MicrosliceView& mc);
+struct StorableMicroslice {
+    StorableMicroslice(const MicrosliceView& mc);
 
-    MicrosliceContainer(MicrosliceDescriptor d, const uint8_t *content);
+    StorableMicroslice(MicrosliceDescriptor d, const uint8_t *content);
     /**<
      * Copy the descriptor and the data pointed to by `content` into the
-     * MicrosliceContainer. The `size` field of the descriptor must already
+     * StorableMicroslice. The `size` field of the descriptor must already
      * be valid and will not be modified.
      */
 
-    MicrosliceContainer(MicrosliceDescriptor d, std::vector<uint8_t> content);
+    StorableMicroslice(MicrosliceDescriptor d, std::vector<uint8_t> content);
     /**<
      * Copy the descriptor and copy or move the data contained in
-     * `content` into the MicrosliceContainer. The descriptor will be
+     * `content` into the StorableMicroslice. The descriptor will be
      * updated to match the size of the `content` vector.
      *
      * Copying the vector is avoided if it is passed as an rvalue,
      * like in
-     *     MicrosliceContainer {..., std::move(some_vector)}
+     *     StorableMicroslice {..., std::move(some_vector)}
      * or
-     *     MicrosliceContainer {..., {1, 2, 3, 4, 5}}
+     *     StorableMicroslice {..., {1, 2, 3, 4, 5}}
      * or
-     *     MicrosliceContainer {..., create_some_vector()}
+     *     StorableMicroslice {..., create_some_vector()}
      */
 
     const MicrosliceDescriptor& desc() { return _desc; };
