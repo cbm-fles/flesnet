6027f74d4f8c2a55dd4c0554861f90258d66d607
cbm <cbm@cbmflib03.gsi.de>
2014-10-07 10:41:02 +0200

    Improved link_reset

--- app/flib_debug/link_reset.cpp
+++ app/flib_debug/link_reset.cpp
@@ -21,13 +21,21 @@ flib_device* MyFlib = NULL;
 
 int main(int argc, char *argv[])
 {
+  if (argc != 2) {
+    printf("Usage: link_reset <link number>\n");
+    return -1;
+  }
   size_t link_nr = atoi(argv[1]);
   
-  if (link_nr < 8) {printf("./link number invalid\n"); return -1;}
+  if (link_nr > 7) {
+    printf("Invalid link number (valid links: 0..7)\n");
+    return -1;
+  }
       
   MyFlib = new flib_device(0);
 
-  MyFlib->link(0).rst_cnet_link();
+  printf("reseting CBMnet link %lu\n", link_nr);
+  MyFlib->link(link_nr).rst_cnet_link();
 
   if (MyFlib) delete MyFlib;
   
