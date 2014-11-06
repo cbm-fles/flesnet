65da0b612e97389ce76b6662f5daea5c01eb8efd
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-11-06 16:33:16 +0100

    used correct functions for dlm prepare & send

--- app/flib_sync/flib_sync.cpp
+++ app/flib_sync/flib_sync.cpp
@@ -1,11 +1,13 @@
 #include "flib.h"
 
+static const uint8_t DLM {1};
+
 int main()
 {
     auto flib = flib_device {0};
 
     for (auto link : flib.links()) {
-        link.prepare_dlm(1); // TODO
+        link->prepare_dlm(DLM, true);
     }
-    flib.send_dlm_global(); // TODO
+    flib.send_dlm();
 }
