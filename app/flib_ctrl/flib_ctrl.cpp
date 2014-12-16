88861d04e19c250a231a181ae35c23b53d9df5c9
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2014-12-16 22:53:52 +0100

    Stripped flib_ctrl

--- app/flib_ctrl/flib_ctrl.cpp
+++ app/flib_ctrl/flib_ctrl.cpp
@@ -2,13 +2,9 @@
 
 #include <zmq.hpp>
 #include <boost/lexical_cast.hpp>
-#ifdef CNETCNTLSERVER
-#include <control/libserver/ControlServer.hpp>
-#endif
 
 #include <log.hpp>
 #include "parameters.hpp"
-#include "flib_server.hpp"
 
 int s_interrupted = 0;
 static void s_signal_handler (int signal_value)
@@ -47,13 +43,6 @@ int main(int argc, char* argv[])
               << (flib.rf()->reg(RORC_REG_MC_CNT_CFG) & 0x7FFFFFFF);
 
   // FLIB per link configuration
-#ifdef CNETCNTLSERVER
-  std::vector<std::unique_ptr<flib_server>> flibserver;
-  std::vector<std::unique_ptr<CbmNet::ControlServer>> ctrlserver;
-#else
-  L_(info) << "Compiled without controls support. Configuring FLIB and exit.";
-#endif
-
   for (size_t i = 0; i < flib.number_of_links(); ++i) {
     L_(debug) << "Initializing link " << i;
 
@@ -62,34 +51,8 @@ int main(int argc, char* argv[])
     links.at(i)->set_hdr_config(&link_config.hdr_config);
     links.at(i)->set_data_sel(link_config.rx_sel);
     links.at(i)->enable_cbmnet_packer_debug_mode(par.debug_mode());
-
-#ifdef CNETCNTLSERVER
-    // create device control server, initialize and start server thread
-    flibserver.push_back(std::unique_ptr<flib_server>(
-        new flib_server(zmq_context, "CbmNet::Driver" + 
-                        boost::lexical_cast<std::string>(i), 
-                        flib, *links.at(i))));
-    flibserver.at(i)->Bind();
-    flibserver.at(i)->Start();
-
-    // create control server and start
-    ctrlserver.push_back(std::unique_ptr<CbmNet::ControlServer>(
-         new CbmNet::ControlServer(zmq_context, "CbmNet::Driver" 
-                                   + boost::lexical_cast<std::string>(i))));
-    //ctrlserver.at(i)->SetDebugFlags(CbmNet::ControlServer::kDbgDumpRpc);    
-    ctrlserver.at(i)->Bind("tcp://*:" + boost::lexical_cast<std::string>(9750 + i));
-    ctrlserver.at(i)->ConnectDriver();
-    ctrlserver.at(i)->Start();
-#endif
   }
 
-#ifdef CNETCNTLSERVER
-  L_(info) << "FLIB configured successfully. Waiting to forward control commands. Send ctrl+c to exit.";
-  // main loop
-  while(s_interrupted==0) {
-    ::sleep(1);
-  }
-#endif
   L_(debug) << "Exiting";
 
   return 0;
