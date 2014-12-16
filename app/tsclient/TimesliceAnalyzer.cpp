84b87bb5ac1c7ec8a83b8f5a94838a61a3ccbd63
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2014-12-16 23:26:25 +0100

    Adopted tsanalyzer to pgen only design

--- app/tsclient/TimesliceAnalyzer.cpp
+++ app/tsclient/TimesliceAnalyzer.cpp
@@ -32,95 +32,13 @@ bool TimesliceAnalyzer::check_flesnet_pattern(
     return true;
 }
 
-bool TimesliceAnalyzer::check_content_pgen(const uint16_t* content, size_t size)
-{
-    constexpr uint16_t source_address = 0;
-
-    if (content[0] != source_address) {
-        std::cerr << "unexpected source address: " << content[0] << std::endl;
-        return false;
-    }
-
-    for (size_t i = 1; i < size - 1; ++i) {
-        uint8_t low = content[i] & 0xff;
-        uint8_t high = (content[i] >> 8) & 0xff;
-        if (high != 0xbc || low != i - 1) {
-            std::cerr << "unexpected cbmnet content word: " << content[i]
-                      << std::endl;
-            return false;
-        }
-    }
-
-    uint16_t pgen_sequence_number = content[size - 1];
-    uint16_t expected_pgen_sequence_number = _pgen_sequence_number + 1;
-    // uncomment to check only for increasing sequence numbers
-    //    if (_pgen_sequence_number != 0 && pgen_sequence_number != 0 &&
-    //        pgen_sequence_number < expected_pgen_sequence_number) {
-    if (_pgen_sequence_number != 0 &&
-        pgen_sequence_number != expected_pgen_sequence_number) {
-        std::cerr << "unexpected pgen sequence number in frame "
-                  << static_cast<unsigned>(_frame_number) << ":  expected "
-                  << expected_pgen_sequence_number << "  found "
-                  << pgen_sequence_number << std::endl;
-        return false;
-    }
-    _pgen_sequence_number = pgen_sequence_number;
-
-    return true;
-}
-
-bool TimesliceAnalyzer::check_cbmnet_frames(const uint16_t* content,
-                                            size_t size, uint8_t sys_id,
-                                            uint8_t sys_ver)
-{
-    size_t i = 0;
-    while (i < size) {
-        uint8_t frame_number = (content[i] >> 8) & 0xff;
-        uint8_t word_count =
-            (content[i] & 0xff) + 1; // FIXME: Definition in hardware
-        uint8_t padding_count = (4 - ((word_count + 1) & 0x3)) & 0x3;
-        ++i;
-
-        uint8_t expected_frame_number = _frame_number + 1;
-        if (_frame_number != 0 && frame_number != expected_frame_number) {
-            std::cerr << "unexpected cbmnet frame number:"
-                      << "  expected: "
-                      << static_cast<uint32_t>(expected_frame_number)
-                      << "  found: " << static_cast<uint32_t>(frame_number)
-                      << std::endl;
-            return false;
-        }
-        _frame_number = frame_number;
-
-        if (word_count < 4 || word_count > 64 ||
-            i + word_count + padding_count > size) {
-            std::cerr << "invalid cbmnet frame word count: " << word_count
-                      << std::endl;
-            return false;
-        }
-
-        if (sys_id == static_cast<uint8_t>(0xF0) &&
-            sys_ver == static_cast<uint8_t>(0x1)) {
-            if (check_content_pgen(&content[i], word_count) == false)
-                return false;
-        }
-        i += word_count + padding_count;
-    }
-
-    return true;
-}
-
 bool TimesliceAnalyzer::check_flib_pattern(
-    const fles::MicrosliceDescriptor& descriptor, const uint64_t* content,
-    size_t /* component */)
+                                           const fles::MicrosliceDescriptor& /* descriptor */,
+                                           const uint64_t* /* content */,
+                                           size_t /* component */)
 {
-    if (content[0] != reinterpret_cast<const uint64_t*>(&descriptor)[0] ||
-        content[1] != reinterpret_cast<const uint64_t*>(&descriptor)[1]) {
-        return false;
-    }
-    return check_cbmnet_frames(reinterpret_cast<const uint16_t*>(&content[2]),
-                               (descriptor.size - 16) / sizeof(uint16_t),
-                               descriptor.sys_id, descriptor.sys_ver);
+    // TODO
+    return true;
 }
 
 bool TimesliceAnalyzer::check_microslice(
