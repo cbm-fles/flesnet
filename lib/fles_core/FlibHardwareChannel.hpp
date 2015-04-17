3b7d8910e74aa726d3a6241d9a74795b445f4e6b
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-04-17 15:46:54 +0200

    Use page-aligned RingBuffer in FlibHardwareChannel.

--- lib/fles_core/FlibHardwareChannel.hpp
+++ lib/fles_core/FlibHardwareChannel.hpp
@@ -76,8 +76,8 @@ private:
     std::unique_ptr<RingBufferView<volatile fles::MicrosliceDescriptor>>
         _desc_buffer_view;
 
-    RingBuffer<volatile uint8_t> _data_send_buffer;
-    RingBuffer<volatile fles::MicrosliceDescriptor> _desc_send_buffer;
+    RingBuffer<volatile uint8_t, true, true> _data_send_buffer;
+    RingBuffer<volatile fles::MicrosliceDescriptor, true, true> _desc_send_buffer;
 
     RingBufferView<volatile uint8_t> _data_send_buffer_view;
     RingBufferView<volatile fles::MicrosliceDescriptor> _desc_send_buffer_view;
