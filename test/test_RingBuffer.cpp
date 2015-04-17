12a6205f214c46a8b39fb0f99c606cef4b9735e3
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-04-17 15:48:43 +0200

    Add simple RingBuffer test application.

---
+++ test/test_RingBuffer.cpp
@@ -0,0 +1,27 @@
+// Copyright 2015 Jan de Cuveland <cmail@cuveland.de>
+
+#include "RingBuffer.hpp"
+#include <iostream>
+
+class Simple
+{
+public:
+    Simple(int n = 5) : i(n) {
+        std::cout << "Simple()\n";
+    }
+    ~Simple() {
+        std::cout << "~Simple()\n";
+    }
+    int i;
+};
+
+int main() {
+    
+    RingBuffer<Simple, false, true> s;
+
+    s.alloc_with_size(4);
+
+    std::printf("ptr: %p\n", static_cast<void*>(s.ptr()));
+
+    return 0;
+}
