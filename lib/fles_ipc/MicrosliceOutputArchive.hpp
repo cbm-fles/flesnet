e7bf564c016b030c97cf798c267431ec747dbc95
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-05-19 18:56:01 +0200

    Use OutputArchive instead of previous MicrosliceOutputArchive.

--- lib/fles_ipc/MicrosliceOutputArchive.hpp
+++ lib/fles_ipc/MicrosliceOutputArchive.hpp
@@ -1,40 +1,14 @@
-// Copyright 2013 Jan de Cuveland <cmail@cuveland.de>
+// Copyright 2015 Jan de Cuveland <cmail@cuveland.de>
 #pragma once
 
-#include "ArchiveDescriptor.hpp"
+#include "OutputArchive.hpp"
 #include "StorableMicroslice.hpp"
-#include <string>
-#include <fstream>
-#include <boost/archive/binary_oarchive.hpp>
 
 namespace fles
 {
 
-//! The MicrosliceOutputArchive serializes microslice data sets to an output
-// file.
-class MicrosliceOutputArchive
-{
-public:
-    MicrosliceOutputArchive(const std::string& filename)
-        : _ofstream(filename, std::ios::binary), _oarchive(_ofstream)
-    {
-        _oarchive << _descriptor;
-    }
-
-    MicrosliceOutputArchive(const MicrosliceOutputArchive&) = delete;
-    void operator=(const MicrosliceOutputArchive&) = delete;
-
-    /// Store a microslice.
-    void write(const StorableMicroslice& microslice)
-    {
-        _oarchive << microslice;
-    }
-
-private:
-    std::ofstream _ofstream;
-    boost::archive::binary_oarchive _oarchive;
-    ArchiveDescriptor _descriptor{
-        ArchiveDescriptor::ArchiveType::MicrosliceArchive};
-};
+typedef OutputArchive<StorableMicroslice,
+                      ArchiveDescriptor::ArchiveType::MicrosliceArchive>
+    MicrosliceOutputArchive;
 
 } // namespace fles {
