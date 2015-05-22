f42ef17123eb7161cbd30e56099b35b0e80f7fe2
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-05-22 16:17:33 +0200

    Experiment with CTest/CDash integration.

---
+++ CTestConfig.cmake
@@ -0,0 +1,5 @@
+set(CTEST_PROJECT_NAME "fles_ipc")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=fles_ipc")
+set(CTEST_DROP_SITE_CDASH TRUE)
