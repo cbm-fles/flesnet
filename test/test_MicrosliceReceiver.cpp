35e96b5710da50e1029fd70b16b4c29671fe7ebc
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-07-23 10:42:46 +0200

    Add boilerplate for new test_MicrosliceReceiver.

---
+++ test/test_MicrosliceReceiver.cpp
@@ -0,0 +1,7 @@
+// Copyright 2015 Jan de Cuveland <cmail@cuveland.de>
+#define BOOST_TEST_MODULE test_MicrosliceReceiver
+#include <boost/test/unit_test.hpp>
+
+#include "MicrosliceReceiver.hpp"
+
+BOOST_AUTO_TEST_CASE(dummy_test) { BOOST_CHECK_EQUAL(1, 1); }
