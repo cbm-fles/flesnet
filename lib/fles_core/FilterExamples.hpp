8f0a19d8aa7b0b35052819f5af8e729a0bee6de1
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2016-03-04 15:49:13 +0100

    Add FilterExamples classes and corresponding tests.

---
+++ lib/fles_core/FilterExamples.hpp
@@ -0,0 +1,67 @@
+// Copyright 2016 Jan de Cuveland <cmail@cuveland.de>
+/// \file
+/// \brief Example microslice stream filters based on fles::Filter.
+#pragma once
+
+#include "Filter.hpp"
+#include "Microslice.hpp"
+#include "StorableMicroslice.hpp"
+
+namespace fles
+{
+
+// Example filter 1: Override system ID in descriptor
+class DescriptorOverrideFilter : public Filter<Microslice>
+{
+public:
+    uint8_t sys_id_;
+    uint8_t sys_ver_;
+
+    DescriptorOverrideFilter(uint8_t sys_id, uint8_t sys_ver)
+        : sys_id_(sys_id), sys_ver_(sys_ver)
+    {
+    }
+
+    virtual std::pair<std::unique_ptr<Microslice>, bool>
+    exchange_item(const Microslice* item) override
+    {
+        if (!item) {
+            return std::make_pair(std::unique_ptr<Microslice>(nullptr), false);
+        }
+        StorableMicroslice* m = new StorableMicroslice(*item);
+
+        // Modify microslice descriptor
+        m->desc().sys_id = sys_id_;
+        m->desc().sys_ver = sys_ver_;
+
+        return std::make_pair(std::unique_ptr<Microslice>(m), false);
+    }
+};
+
+// Example filter 2: Combine microslices
+class CombineContentsFilter
+    : public BufferingFilter<Microslice, StorableMicroslice>
+{
+private:
+    void process() override
+    {
+        // combine the contents of two consecutive microslices
+        while (this->input.size() >= 2) {
+            StorableMicroslice item1 = this->input.front();
+            this->input.pop_front();
+            StorableMicroslice item2 = this->input.front();
+            this->input.pop_front();
+
+            MicrosliceDescriptor desc = item1.desc();
+            desc.size += item2.desc().size;
+            std::vector<uint8_t> content;
+            content.assign(item1.content(),
+                           item1.content() + item1.desc().size);
+            content.insert(content.end(), item2.content(),
+                           item2.content() + item2.desc().size);
+            StorableMicroslice combined(desc, content);
+            this->output.push(combined);
+        }
+    }
+};
+}
