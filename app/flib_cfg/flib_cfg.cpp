1f65c8e8df82fbb48b9a3d318a7e9181acb0df8e
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2016-03-30 20:14:41 +0200

    Added exception catching to flib_cfg.

--- app/flib_cfg/flib_cfg.cpp
+++ app/flib_cfg/flib_cfg.cpp
@@ -26,60 +26,41 @@ int main(int argc, char* argv[]) {
   s_catch_signals();
   int ret = 0;
 
-  parameters par(argc, argv);
+  try {
 
-  // create FLIB
-  flib::flib_device_flesin flib(par.flib());
-  std::vector<flib::flib_link_flesin*> links = flib.links();
-  std::vector<std::unique_ptr<flib::flim>> flims;
+    parameters par(argc, argv);
 
-  L_(info) << "Configuring FLIB " << par.flib() << ": " << flib.print_devinfo();
+    // create FLIB
+    flib::flib_device_flesin flib(par.flib());
+    std::vector<flib::flib_link_flesin*> links = flib.links();
+    std::vector<std::unique_ptr<flib::flim>> flims;
 
-  // FLIB global configuration
-  // set even if unused
-  flib.set_mc_time(par.mc_size());
+    L_(info) << "Configuring FLIB " << par.flib() << ": "
+             << flib.print_devinfo();
 
-  L_(debug) << "Tatal FLIB links: " << flib.number_of_links();
+    // FLIB global configuration
+    // set even if unused
+    flib.set_mc_time(par.mc_size());
 
-  // FLIB per link configuration
-  for (size_t i = 0; i < flib.number_of_links(); ++i) {
-    L_(debug) << "Initializing link " << i;
+    L_(debug) << "Tatal FLIB links: " << flib.number_of_links();
 
-    if (par.link(i).source == disable) {
-      links.at(i)->set_data_sel(flib::flib_link::rx_disable);
-    } else if (par.link(i).source == pgen_near) {
-      links.at(i)->set_data_sel(flib::flib_link::rx_pgen);
-      // create internal FLIM
-      try {
-        flims.push_back(
-            std::unique_ptr<flib::flim>(new flib::flim(links.at(i))));
-      } catch (const std::exception& e) {
-        L_(error) << e.what();
-        exit(EXIT_FAILURE);
-      }
-      flims.back()->reset_datapath();
-      if (!flims.back()->get_pgen_present()) {
-        L_(error) << "FLIM build does not support pgen";
-        exit(EXIT_FAILURE);
-      }
-      flims.back()->set_pgen_mc_size(par.mc_size());
-      flims.back()->set_pgen_rate(par.pgen_rate());
-      flims.back()->set_pgen_ids(par.link(i).eq_id);
-      flims.back()->set_data_source(flib::flim::pgen);
-    } else if (par.link(i).source == flim || par.link(i).source == pgen_far) {
-      links.at(i)->set_data_sel(flib::flib_link::rx_link);
-      // create FLIM
-      try {
-        flims.push_back(
-            std::unique_ptr<flib::flim>(new flib::flim(links.at(i))));
-      } catch (const std::exception& e) {
-        L_(error) << e.what();
-        exit(EXIT_FAILURE);
-      }
-      flims.back()->reset_datapath();
-      if (par.link(i).source == flim) {
-        flims.back()->set_data_source(flib::flim::user);
-      } else { // pgen_far
+    // FLIB per link configuration
+    for (size_t i = 0; i < flib.number_of_links(); ++i) {
+      L_(debug) << "Initializing link " << i;
+
+      if (par.link(i).source == disable) {
+        links.at(i)->set_data_sel(flib::flib_link::rx_disable);
+      } else if (par.link(i).source == pgen_near) {
+        links.at(i)->set_data_sel(flib::flib_link::rx_pgen);
+        // create internal FLIM
+        try {
+          flims.push_back(
+              std::unique_ptr<flib::flim>(new flib::flim(links.at(i))));
+        } catch (const std::exception& e) {
+          L_(error) << e.what();
+          exit(EXIT_FAILURE);
+        }
+        flims.back()->reset_datapath();
         if (!flims.back()->get_pgen_present()) {
           L_(error) << "FLIM build does not support pgen";
           exit(EXIT_FAILURE);
@@ -88,11 +69,37 @@ int main(int argc, char* argv[]) {
         flims.back()->set_pgen_rate(par.pgen_rate());
         flims.back()->set_pgen_ids(par.link(i).eq_id);
         flims.back()->set_data_source(flib::flim::pgen);
+      } else if (par.link(i).source == flim || par.link(i).source == pgen_far) {
+        links.at(i)->set_data_sel(flib::flib_link::rx_link);
+        // create FLIM
+        try {
+          flims.push_back(
+              std::unique_ptr<flib::flim>(new flib::flim(links.at(i))));
+        } catch (const std::exception& e) {
+          L_(error) << e.what();
+          exit(EXIT_FAILURE);
+        }
+        flims.back()->reset_datapath();
+        if (par.link(i).source == flim) {
+          flims.back()->set_data_source(flib::flim::user);
+        } else { // pgen_far
+          if (!flims.back()->get_pgen_present()) {
+            L_(error) << "FLIM build does not support pgen";
+            exit(EXIT_FAILURE);
+          }
+          flims.back()->set_pgen_mc_size(par.mc_size());
+          flims.back()->set_pgen_rate(par.pgen_rate());
+          flims.back()->set_pgen_ids(par.link(i).eq_id);
+          flims.back()->set_data_source(flib::flim::pgen);
+        }
       }
     }
-  }
 
-  L_(debug) << "Exiting";
+    L_(debug) << "Exiting";
+
+  } catch (std::exception const& e) {
+    L_(fatal) << "exception: " << e.what();
+  }
 
   return ret;
 }
