549bb15d54d45fca57c39fbf5637a34e974d3ec9
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2016-03-30 19:38:38 +0200

    Improved flib_info.

--- app/flib_tools/flib_info.cpp
+++ app/flib_tools/flib_info.cpp
@@ -20,15 +20,18 @@ int main() {
   std::cout << "Total number of FLIBs: " << num_dev << std::endl;
 
   for (size_t i = 0; i < num_dev; ++i) {
-    flibs.push_back(
-        std::unique_ptr<flib_device_flesin>(new flib_device_flesin(i)));
-
     std::cout << "*** FLIB " << i << " ***" << std::endl;
-    std::cout << flibs.back()->print_devinfo() << std::endl;
-    std::cout << "Hardware links: "
-              << static_cast<unsigned>(flibs.back()->number_of_hw_links())
-              << std::endl;
-    std::cout << flibs.back()->print_build_info() << std::endl;
+    try {
+      flibs.push_back(
+          std::unique_ptr<flib_device_flesin>(new flib_device_flesin(i)));
+      std::cout << flibs.back()->print_devinfo() << std::endl;
+      std::cout << "Hardware links: "
+                << static_cast<unsigned>(flibs.back()->number_of_hw_links())
+                << std::endl;
+      std::cout << flibs.back()->print_build_info() << std::endl;
+    } catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
   }
 
   return 0;
