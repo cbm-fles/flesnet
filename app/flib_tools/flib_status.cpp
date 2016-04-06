40043bb0bae18479170f03e1b4267cbd675ac2f0
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2016-04-06 18:21:20 +0200

    Multi flib version for flib_status.

--- app/flib_tools/flib_status.cpp
+++ app/flib_tools/flib_status.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "flib.h"
+#include "device_operator.hpp"
 #include <iostream>
 
 std::ostream& operator<<(std::ostream& os, flib::flib_link::data_sel_t sel) {
@@ -27,44 +28,53 @@ std::ostream& operator<<(std::ostream& os, flib::flib_link::data_sel_t sel) {
   return os;
 }
 
-std::unique_ptr<flib::flib_device_flesin> Flib;
-
-int main(int argc, char* argv[]) {
-
-  size_t flib_index = 0;
-  if (argc == 2) {
-    flib_index = atoi(argv[1]);
-    std::cout << "Status FLIB " << flib_index << std::endl;
-  } else {
-    std::cout << "usage: " << argv[0] << " <flib-index>" << std::endl;
-    exit(EXIT_FAILURE);
-  }
+int main() {
 
   try {
-    Flib = std::unique_ptr<flib::flib_device_flesin>(
-        new flib::flib_device_flesin(flib_index));
+    std::unique_ptr<pda::device_operator> dev_op(new pda::device_operator);
+    std::vector<std::unique_ptr<flib::flib_device_flesin>> flibs;
+    uint64_t num_dev = dev_op->device_count();
+
+    for (size_t i = 0; i < num_dev; ++i) {
+      flibs.push_back(std::unique_ptr<flib::flib_device_flesin>(
+          new flib::flib_device_flesin(i)));
+    }
+
+    size_t j = 0;
+    for (auto& flib : flibs) {
+      std::cout << "Flib " << j << ": " << flib->print_devinfo() << std::endl;
+      ++j;
+    }
+
+    std::cout << "\nlink  data_sel  channel_up  hard_err  soft_err  "
+                 "eoe_ovfl  d_ovfl  d_max_words\n";
+    j = 0;
+    for (auto& flib : flibs) {
+      size_t num_links = flib->number_of_hw_links();
+      std::vector<flib::flib_link_flesin*> links = flib->links();
+
+      if (j != 0) {
+        std::cout << "\n";
+      }
+      std::stringstream ss;
+      for (size_t i = 0; i < num_links; ++i) {
+        ss << std::setw(2) << j << "/" << i << "  " << std::setw(8)
+           << links.at(i)->data_sel() << "  " << std::setw(10)
+           << links.at(i)->link_status().channel_up << "  " << std::setw(8)
+           << links.at(i)->link_status().hard_err << "  " << std::setw(8)
+           << links.at(i)->link_status().soft_err << "  " << std::setw(8)
+           << links.at(i)->link_status().eoe_fifo_overflow << "  "
+           << std::setw(6) << links.at(i)->link_status().d_fifo_overflow << "  "
+           << std::setw(11) << links.at(i)->link_status().d_fifo_max_words
+           << "\n";
+      }
+      std::cout << ss.str();
+      ++j;
+    }
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
     exit(EXIT_FAILURE);
   }
-
-  size_t num_links = Flib->number_of_hw_links();
-  std::cout << "Total number of links: " << num_links << std::endl;
-  std::vector<flib::flib_link_flesin*> links = Flib->links();
-
-  std::stringstream ss;
-  ss << "link  data_sel  channel_up  hard_err  soft_err  "
-        "eoe_ovfl  d_ovfl  d_max_words\n";
-  for (size_t i = 0; i < num_links; ++i) {
-    ss << std::setw(4) << i << "  " << std::setw(8) << links.at(i)->data_sel()
-       << "  " << std::setw(10) << links.at(i)->link_status().channel_up << "  "
-       << std::setw(8) << links.at(i)->link_status().hard_err << "  "
-       << std::setw(8) << links.at(i)->link_status().soft_err << "  "
-       << std::setw(8) << links.at(i)->link_status().eoe_fifo_overflow << "  "
-       << std::setw(6) << links.at(i)->link_status().d_fifo_overflow << "  "
-       << std::setw(11) << links.at(i)->link_status().d_fifo_max_words << "\n";
-  }
-  std::cout << ss.str();
 
   return EXIT_SUCCESS;
 }
