f10f1b14c08449e4f4758149a1b965a8c80d50b9
Thorsten Schuett <schuett@zib.de>
2016-06-08 14:07:45 +0200

    remove unused variable

--- lib/fles_libfabric/VerbsProvider.hpp
+++ lib/fles_libfabric/VerbsProvider.hpp
@@ -35,7 +35,6 @@ public:
                 fid_eq *eq) override;
 
     void connect(fid_ep *ep,
-                 uint_fast16_t remote_index,
                  uint32_t max_send_wr, uint32_t max_send_sge,
                  uint32_t max_recv_wr, uint32_t max_recv_sge,
                  uint32_t max_inline_data, const void *param, size_t paramlen) override;
