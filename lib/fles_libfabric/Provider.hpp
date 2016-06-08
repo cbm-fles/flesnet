421d101f9652dfda58f264222c2e9eaf09f8f3df
Thorsten Schuett <schuett@zib.de>
2016-06-08 14:07:45 +0200

    remove unused variable

--- lib/fles_libfabric/Provider.hpp
+++ lib/fles_libfabric/Provider.hpp
@@ -23,7 +23,6 @@ public:
                         unsigned int count, fid_eq *eq) = 0;
 
     virtual void connect(fid_ep *ep,
-                         uint_fast16_t remote_index,
                          uint32_t max_send_wr, uint32_t max_send_sge,
                          uint32_t max_recv_wr, uint32_t max_recv_sge,
                          uint32_t max_inline_data, const void *param,
