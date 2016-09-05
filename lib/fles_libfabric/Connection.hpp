134cf2ee321ce458037cad7b810179c3e7ea8bb8
Thorsten Schuett <schuett@zib.de>
2016-09-05 16:18:09 +0200

    added avs to the connection class
    better handling of empty strings ("") in fi_getinfo calls
    make_endpoint checks whether it has to bind avs and eqs to endpoints

--- lib/fles_libfabric/Connection.hpp
+++ lib/fles_libfabric/Connection.hpp
@@ -20,11 +20,11 @@ class Connection
 {
 public:
     /// The Connection constructor. Creates an endpoint.
-  Connection(struct fid_eq *eq, uint_fast16_t connection_index,
+    Connection(struct fid_eq* eq, uint_fast16_t connection_index,
                uint_fast16_t remote_connection_index);
 
-    Connection(const Connection &) = delete;
-    Connection &operator=(const Connection &) = delete;
+    Connection(const Connection&) = delete;
+    Connection& operator=(const Connection&) = delete;
 
     /// The Connection destructor.
     virtual ~Connection();
@@ -37,23 +37,23 @@ public:
        \param hostname The target hostname
        \param service  The target service or port number
     */
-  void connect(const std::string &hostname, const std::string &service,
-               struct fid_domain *domain,
-                         struct fid_cq *cq);
+    void connect(const std::string& hostname, const std::string& service,
+                 struct fid_domain* domain, struct fid_cq* cq,
+                 struct fid_av* av);
 
     void disconnect();
 
-    virtual void on_rejected(struct fi_eq_err_entry *event);
+    virtual void on_rejected(struct fi_eq_err_entry* event);
 
     /// Connection handler function, called on successful connection.
     /**
        \param event RDMA connection manager event structure
        \return      Non-zero if an error occured
     */
-    virtual void on_established(struct fi_eq_cm_entry *event);
+    virtual void on_established(struct fi_eq_cm_entry* event);
 
     /// Handle RDMA_CM_EVENT_DISCONNECTED event for this connection.
-    virtual void on_disconnected(struct fi_eq_cm_entry *event);
+    virtual void on_disconnected(struct fi_eq_cm_entry* event);
     //
     //    /// Handle RDMA_CM_EVENT_ADDR_RESOLVED event for this connection.
     //    virtual void on_addr_resolved(struct ibv_pd* pd, struct ibv_cq* cq);
@@ -63,63 +63,49 @@ public:
     // virtual void accept_connect_request();
 
     /// Handle RDMA_CM_EVENT_CONNECT_REQUEST event for this connection.
-  virtual void on_connect_request(struct fi_eq_cm_entry *event,
-                                  struct fid_domain *pd, struct fid_cq *cq);
+    virtual void on_connect_request(struct fi_eq_cm_entry* event,
+                                    struct fid_domain* pd, struct fid_cq* cq);
 
-    virtual std::unique_ptr<std::vector<uint8_t> > get_private_data();
+    virtual std::unique_ptr<std::vector<uint8_t>> get_private_data();
 
     virtual void setup() = 0;
-    virtual void setup_mr(struct fid_domain *pd) = 0;
+    virtual void setup_mr(struct fid_domain* pd) = 0;
     //
     //    /// Handle RDMA_CM_EVENT_ROUTE_RESOLVED event for this connection.
     //    virtual void on_route_resolved();
     //
     /// Retrieve index of this connection in the local connection group.
-    uint_fast16_t index() const
-    {
-        return index_;
-    }
+    uint_fast16_t index() const { return index_; }
 
     /// Retrieve index of this connection in the remote connection group.
-    uint_fast16_t remote_index() const
-    {
-        return remote_index_;
-    }
+    uint_fast16_t remote_index() const { return remote_index_; }
 
-    bool done() const
-    {
-        return done_;
-    }
+    bool done() const { return done_; }
 
     /// Retrieve the total number of bytes transmitted.
-    uint64_t total_bytes_sent() const
-    {
-        return total_bytes_sent_;
-    }
+    uint64_t total_bytes_sent() const { return total_bytes_sent_; }
 
     /// Retrieve the total number of SEND work requests.
-    uint64_t total_send_requests() const
-    {
-        return total_send_requests_;
-    }
+    uint64_t total_send_requests() const { return total_send_requests_; }
 
     /// Retrieve the total number of RECV work requests.
-    uint64_t total_recv_requests() const
-    {
-        return total_recv_requests_;
-    }
+    uint64_t total_recv_requests() const { return total_recv_requests_; }
     //
 protected:
     //    void dump_send_wr(struct ibv_send_wr* wr);
 
     /// Post an Libfabric rdma send work request
-    void post_send_rdma(struct fi_msg_rma *msg, uint64_t flags);
+    void post_send_rdma(struct fi_msg_rma* msg, uint64_t flags);
 
     /// Post an Libfabric message send work request
-    void post_send_msg(struct fi_msg *msg);
+    void post_send_msg(struct fi_msg* msg);
 
     /// Post an Libfabric message recveive request.
-    void post_recv_msg(const struct fi_msg *msg);
+    void post_recv_msg(const struct fi_msg* msg);
+
+    void make_endpoint(struct fi_info* info, const std::string& hostname,
+                       const std::string& service, struct fid_domain* pd,
+                       struct fid_cq* cq, struct fid_av* av);
 
     /// Index of this connection in the local group of connections.
     uint_fast16_t index_;
@@ -140,6 +126,8 @@ protected:
     uint32_t max_recv_sge_;
     uint32_t max_inline_data_;
 
+    struct fid_ep* ep_ = nullptr;
+
 private:
     //    /// Low-level communication parameters.
     //    enum {
@@ -149,9 +137,8 @@ private:
     //
     /// RDMA connection manager ID.
     //    struct rdma_cm_id* cm_id_ = nullptr;
-  struct fid_eq *eq_ = nullptr;
-  //struct fid_pep *pep_ = nullptr;
-    struct fid_ep *ep_ = nullptr;
+    struct fid_eq* eq_ = nullptr;
+    // struct fid_pep *pep_ = nullptr;
 
     /// Total number of bytes transmitted.
     uint64_t total_bytes_sent_ = 0;
