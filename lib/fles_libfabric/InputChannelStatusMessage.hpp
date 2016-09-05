ea675af0878a6a82726332ffb786307b89c5e896
Thorsten Schuett <schuett@zib.de>
2016-09-05 16:21:58 +0200

    add connect and address info to InputChannelStatusMessage
    The InputChannelStatusMessage is used for connection-less fabrics to
    start a "connection". It sends the endpoint address and the private
    data of the Verbs fabric

--- lib/fles_libfabric/InputChannelStatusMessage.hpp
+++ lib/fles_libfabric/InputChannelStatusMessage.hpp
@@ -2,16 +2,20 @@
 #pragma once
 
 #include "ComputeNodeBufferPosition.hpp"
+#include "InputNodeInfo.hpp"
 
 #pragma pack(1)
 
 /// Structure representing a status update message sent from input channel to
 /// compute buffer.
-struct InputChannelStatusMessage
-{
+struct InputChannelStatusMessage {
     ComputeNodeBufferPosition wp;
     bool abort;
     bool final;
+    // "private data" on connect
+    bool connect;
+    InputNodeInfo info;
+    unsigned char my_address[64]; // gni: 50?};
 };
 
 #pragma pack()
