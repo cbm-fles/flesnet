8a5c187b8ab026038e827a8f267bf43538663101
Thorsten Schuett <schuett@zib.de>
2016-09-05 18:00:20 +0200

    adapt ComputeNodeConnection for connection-less protocols
    two constructors (one for connection-oriented fabrics and one for
    connection-less fabrics) the second one requires a protection domain,
    a completion queue and and address vector to create an anonymous endpoint.
    
    set_partner_addr method is used to set the av entry of the other end of the connection
    
    update ComputeNodeStatusMessage include ComputeNodeInfo

--- lib/fles_libfabric/ComputeNodeConnection.cpp
+++ lib/fles_libfabric/ComputeNodeConnection.cpp
@@ -9,12 +9,11 @@
 #include <log.hpp>
 
 ComputeNodeConnection::ComputeNodeConnection(
-                                             struct fid_eq *eq,
-                                             uint_fast16_t connection_index,
-                                             uint_fast16_t remote_connection_index,
-    InputNodeInfo remote_info, uint8_t *data_ptr, uint32_t data_buffer_size_exp,
-    fles::TimesliceComponentDescriptor *desc_ptr, uint32_t desc_buffer_size_exp)
-  : Connection(eq, connection_index, remote_connection_index),
+    struct fid_eq* eq, uint_fast16_t connection_index,
+    uint_fast16_t remote_connection_index, InputNodeInfo remote_info,
+    uint8_t* data_ptr, uint32_t data_buffer_size_exp,
+    fles::TimesliceComponentDescriptor* desc_ptr, uint32_t desc_buffer_size_exp)
+    : Connection(eq, connection_index, remote_connection_index),
       remote_info_(std::move(remote_info)), data_ptr_(data_ptr),
       data_buffer_size_exp_(data_buffer_size_exp), desc_ptr_(desc_ptr),
       desc_buffer_size_exp_(desc_buffer_size_exp)
@@ -25,6 +24,29 @@ ComputeNodeConnection::ComputeNodeConnection(
     max_send_sge_ = 1;
     max_recv_wr_ = 1;
     max_recv_sge_ = 1;
+}
+
+ComputeNodeConnection::ComputeNodeConnection(
+    struct fid_eq* eq, struct fid_domain* pd, struct fid_cq* cq,
+    struct fid_av* av, uint_fast16_t connection_index,
+    uint_fast16_t remote_connection_index,
+    /*InputNodeInfo remote_info, */ uint8_t* data_ptr,
+    uint32_t data_buffer_size_exp, fles::TimesliceComponentDescriptor* desc_ptr,
+    uint32_t desc_buffer_size_exp)
+    : Connection(eq, connection_index, remote_connection_index),
+      data_ptr_(data_ptr), data_buffer_size_exp_(data_buffer_size_exp),
+      desc_ptr_(desc_ptr), desc_buffer_size_exp_(desc_buffer_size_exp)
+{
+
+    // send and receive only single StatusMessage struct
+    max_send_wr_ = 2; // one additional wr to avoid race (recv before
+    // send completion)
+    max_send_sge_ = 1;
+    max_recv_wr_ = 1;
+    max_recv_sge_ = 1;
+
+    //  setup anonymous endpoint
+    make_endpoint(Provider::getInst()->get_info(), "", "", pd, cq, av);
 }
 
 void ComputeNodeConnection::post_recv_status_message()
@@ -45,7 +67,7 @@ void ComputeNodeConnection::post_send_status_message()
                   << "POST SEND status_message"
                   << " (ack.desc=" << send_status_message_.ack.desc << ")";
     }
-    while (pending_send_requests_ >= 2 /*qp_cap_.max_send_wr*/) {
+    while (pending_send_requests_ >= 2000 /*qp_cap_.max_send_wr*/) {
         throw LibfabricException(
             "Max number of pending send requests exceeded");
     }
@@ -95,6 +117,17 @@ void ComputeNodeConnection::setup_mr(struct fid_domain* pd)
     if (!mr_data_ || !mr_desc_ || !mr_recv_ || !mr_send_)
         throw LibfabricException(
             "registration of memory region failed in ComputeNodeConnection");
+
+    send_status_message_.info.data.addr =
+        reinterpret_cast<uintptr_t>(data_ptr_);
+    send_status_message_.info.data.rkey = fi_mr_key(mr_data_);
+    send_status_message_.info.desc.addr =
+        reinterpret_cast<uintptr_t>(desc_ptr_);
+    send_status_message_.info.desc.rkey = fi_mr_key(mr_desc_);
+    send_status_message_.info.index = remote_index_;
+    send_status_message_.info.data_buffer_size_exp = data_buffer_size_exp_;
+    send_status_message_.info.desc_buffer_size_exp = desc_buffer_size_exp_;
+    send_status_message_.connect = false;
 }
 
 void ComputeNodeConnection::setup()
@@ -110,7 +143,7 @@ void ComputeNodeConnection::setup()
     recv_wr.iov_count = 1;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    recv_wr.context = (void *)(ID_RECEIVE_STATUS | (index_ << 8));
+    recv_wr.context = (void*)(ID_RECEIVE_STATUS | (index_ << 8));
 #pragma GCC diagnostic pop
 
     send_sge.iov_base = &send_status_message_;
@@ -123,14 +156,14 @@ void ComputeNodeConnection::setup()
     send_wr.iov_count = 1;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    send_wr.context = (void *)(ID_SEND_STATUS | (index_ << 8));
+    send_wr.context = (void*)(ID_SEND_STATUS | (index_ << 8));
 #pragma GCC diagnostic pop
 
     // post initial receive request
     post_recv_status_message();
 }
 
-void ComputeNodeConnection::on_established(struct fi_eq_cm_entry *event)
+void ComputeNodeConnection::on_established(struct fi_eq_cm_entry* event)
 {
     Connection::on_established(event);
 
@@ -138,29 +171,29 @@ void ComputeNodeConnection::on_established(struct fi_eq_cm_entry *event)
               << "remote index: " << remote_info_.index;
 }
 
-void ComputeNodeConnection::on_disconnected(struct fi_eq_cm_entry *event)
+void ComputeNodeConnection::on_disconnected(struct fi_eq_cm_entry* event)
 {
     disconnect();
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
     if (mr_recv_) {
-        fi_close((struct fid *)mr_recv_);
+        fi_close((struct fid*)mr_recv_);
         mr_recv_ = nullptr;
     }
 
     if (mr_send_) {
-        fi_close((struct fid *)mr_send_);
+        fi_close((struct fid*)mr_send_);
         mr_send_ = nullptr;
     }
 
     if (mr_desc_) {
-        fi_close((struct fid *)mr_desc_);
+        fi_close((struct fid*)mr_desc_);
         mr_desc_ = nullptr;
     }
 
     if (mr_data_) {
-        fi_close((struct fid *)mr_data_);
+        fi_close((struct fid*)mr_data_);
         mr_data_ = nullptr;
     }
 #pragma GCC diagnostic pop
@@ -172,7 +205,7 @@ void ComputeNodeConnection::inc_ack_pointers(uint64_t ack_pos)
 {
     cn_ack_.desc = ack_pos;
 
-    const fles::TimesliceComponentDescriptor &acked_ts =
+    const fles::TimesliceComponentDescriptor& acked_ts =
         desc_ptr_[(ack_pos - 1) & ((UINT64_C(1) << desc_buffer_size_exp_) - 1)];
 
     cn_ack_.data = acked_ts.offset + acked_ts.size;
@@ -201,25 +234,19 @@ void ComputeNodeConnection::on_complete_recv()
     post_send_status_message();
 }
 
-void ComputeNodeConnection::on_complete_send()
-{
-    pending_send_requests_--;
-}
+void ComputeNodeConnection::on_complete_send() { pending_send_requests_--; }
 
-void ComputeNodeConnection::on_complete_send_finalize()
-{
-    done_ = true;
-}
+void ComputeNodeConnection::on_complete_send_finalize() { done_ = true; }
 
-std::unique_ptr<std::vector<uint8_t> > ComputeNodeConnection::get_private_data()
+std::unique_ptr<std::vector<uint8_t>> ComputeNodeConnection::get_private_data()
 {
     assert(data_ptr_ && desc_ptr_ && data_buffer_size_exp_ &&
            desc_buffer_size_exp_);
-    std::unique_ptr<std::vector<uint8_t> > private_data(
+    std::unique_ptr<std::vector<uint8_t>> private_data(
         new std::vector<uint8_t>(sizeof(ComputeNodeInfo)));
 
-    ComputeNodeInfo *cn_info =
-        reinterpret_cast<ComputeNodeInfo *>(private_data->data());
+    ComputeNodeInfo* cn_info =
+        reinterpret_cast<ComputeNodeInfo*>(private_data->data());
     cn_info->data.addr = reinterpret_cast<uintptr_t>(data_ptr_);
     cn_info->data.rkey = fi_mr_key(mr_data_);
     cn_info->desc.addr = reinterpret_cast<uintptr_t>(desc_ptr_);
@@ -229,4 +256,9 @@ std::unique_ptr<std::vector<uint8_t> > ComputeNodeConnection::get_private_data()
     cn_info->desc_buffer_size_exp = desc_buffer_size_exp_;
 
     return private_data;
+}
+
+void ComputeNodeConnection::set_partner_addr(fi_addr_t addr)
+{
+    partner_addr_ = addr;
 }
