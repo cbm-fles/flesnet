96ec8aaa2047bcec8a6a8cb972dc8f4526e4de5d
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2016-11-10 15:54:37 +0100

    Removed unused signal handler from flib_cfg.

--- app/flib_cfg/flib_cfg.cpp
+++ app/flib_cfg/flib_cfg.cpp
@@ -7,24 +7,8 @@
 #include "log.hpp"
 #include "parameters.hpp"
 #include <boost/lexical_cast.hpp>
-#include <csignal>
-
-int s_interrupted = 0;
-static void s_signal_handler(int /* signal_value */) { s_interrupted = 1; }
-
-static void s_catch_signals(void) {
-  struct sigaction action;
-  action.sa_handler = s_signal_handler;
-  action.sa_flags = 0;
-  sigemptyset(&action.sa_mask);
-  sigaction(SIGABRT, &action, NULL);
-  sigaction(SIGTERM, &action, NULL);
-  sigaction(SIGINT, &action, NULL);
-}
 
 int main(int argc, char* argv[]) {
-  s_catch_signals();
-
   try {
 
     parameters par(argc, argv);
