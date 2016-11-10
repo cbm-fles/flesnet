1742b7aad221ec46b3b62b3d0b44d378429c7009
fsalem <farouqsalem@gmail.com>
2016-11-10 18:48:10 +0100

    remove local path in findLibfabric.cmake

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -1,6 +1,4 @@
-# Copyright 2016 Farouk Salem <salem@zib.de>
-
-set(LIBFABRIC_PREFIX /home/farouk/workspace/libfabric)
+// Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
 
 find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h)
 find_library(LIBFABRIC_LIBRARY NAMES fabric)
@@ -10,6 +8,7 @@ set(LIBFABRIC_INCLUDE_DIRS ${LIBFABRIC_INCLUDE_DIR} )
 
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_LIBRARIES}")
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_INCLUDE_DIRS}")
+
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARIES LIBFABRIC_INCLUDE_DIRS)
