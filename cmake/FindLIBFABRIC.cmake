1082e3754552311173d7b85ed6c090e7e0ad43af
fsalem <farouqsalem@gmail.com>
2016-11-10 18:53:29 +0100

    Update '//' with '#' in FindLIBFABRIC.cmake

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -1,4 +1,4 @@
-// Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
+# Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
 
 find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h)
 find_library(LIBFABRIC_LIBRARY NAMES fabric)
@@ -8,7 +8,6 @@ set(LIBFABRIC_INCLUDE_DIRS ${LIBFABRIC_INCLUDE_DIR} )
 
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_LIBRARIES}")
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_INCLUDE_DIRS}")
-
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARIES LIBFABRIC_INCLUDE_DIRS)
