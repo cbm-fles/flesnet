a4ef577d80e448c6af37242e3ff6a910d4fc9971
Farouk Salem <salem@zib.de>
2016-12-12 13:16:26 +0100

    Update FindLIBFABRIC.cmake
    Remove duplicate variables and replace include_directories with target_include_directories

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -1,15 +1,12 @@
 # Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
 
-find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h)
-find_library(LIBFABRIC_LIBRARY NAMES fabric)
-
-set(LIBFABRIC_LIBRARIES ${LIBFABRIC_LIBRARY} )
-set(LIBFABRIC_INCLUDE_DIRS ${LIBFABRIC_INCLUDE_DIR} )
+find_path(LIBFABRIC_INCLUDE_DIRS rdma/fabric.h)
+find_library(LIBFABRIC_LIBRARIES NAMES fabric)
 
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_LIBRARIES}")
 MESSAGE(STATUS "libfabric           = ${LIBFABRIC_INCLUDE_DIRS}")
 
-include_directories(SYSTEM ${LIBFABRIC_INCLUDE_DIRS})
+target_include_directories(${LIBFABRIC_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARIES LIBFABRIC_INCLUDE_DIRS)
