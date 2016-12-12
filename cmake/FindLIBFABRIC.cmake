fd25b78607b44dd40dd2cbe5b83a090cb5e27b12
Farouk Salem <salem@zib.de>
2016-12-12 13:17:35 +0100

    Update FindLIBFABRIC.cmake
    Drop print messages

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -3,9 +3,6 @@
 find_path(LIBFABRIC_INCLUDE_DIRS rdma/fabric.h)
 find_library(LIBFABRIC_LIBRARIES NAMES fabric)
 
-MESSAGE(STATUS "libfabric           = ${LIBFABRIC_LIBRARIES}")
-MESSAGE(STATUS "libfabric           = ${LIBFABRIC_INCLUDE_DIRS}")
-
 target_include_directories(${LIBFABRIC_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
