6f70b51b09ae685ff5b2269b7d9cb47bc8b010d5
fsalem <farouqsalem@gmail.com>
2016-12-12 16:45:22 +0100

    Remove "flesnet" and "public" targets

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -3,7 +3,7 @@
 find_path(LIBFABRIC_INCLUDE_DIRS rdma/fabric.h)
 find_library(LIBFABRIC_LIBRARIES NAMES fabric)
 
-target_include_directories(flesnet SYSTEM PUBLIC ${LIBFABRIC_INCLUDE_DIRS})
+target_include_directories(SYSTEM ${LIBFABRIC_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARIES LIBFABRIC_INCLUDE_DIRS)
