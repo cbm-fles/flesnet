3619a62d6a12eac08fe8f30cbe397bc7713985d0
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2016-12-23 14:01:44 +0100

    Modify to use singular form of LIBFABRIC build variables.

--- cmake/FindLIBFABRIC.cmake
+++ cmake/FindLIBFABRIC.cmake
@@ -1,9 +1,7 @@
 # Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
 
-find_path(LIBFABRIC_INCLUDE_DIRS rdma/fabric.h)
-find_library(LIBFABRIC_LIBRARIES NAMES fabric)
-
-#target_include_directories(PUBLIC ${LIBFABRIC_INCLUDE_DIRS})
+find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h)
+find_library(LIBFABRIC_LIBRARY NAMES fabric)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARIES LIBFABRIC_INCLUDE_DIRS)
+find_package_handle_standard_args(LIBFABRIC REQUIRED_VARS LIBFABRIC_LIBRARY LIBFABRIC_INCLUDE_DIR)
