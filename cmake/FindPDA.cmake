c185b0e23e3435e11ac3e3071945af4d774b7525
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2017-01-27 18:25:42 +0100

    Improved FindPDA cmake script to support version checking.

--- cmake/FindPDA.cmake
+++ cmake/FindPDA.cmake
@@ -1,12 +1,27 @@
 # Copyright 2016 Jan de Cuveland <cmail@cuveland.de>
 
+set(_PDA_PREFIX /opt/pda/${PDA_FIND_VERSION})
+
 find_path(
-	PDA_INCLUDE_DIR pda.h
-  PATHS /opt/pda/${PDA_FIND_VERSION}/include pda/build_linux_x86_uio/include NO_DEFAULT_PATH)
+        PDA_INCLUDE_DIR pda.h
+        PATHS ${_PDA_PREFIX}/include NO_DEFAULT_PATH)
 
 find_library(
-	PDA_LIBRARY pda
-  PATHS /opt/pda/${PDA_FIND_VERSION}/lib pda/build_linux_x86_uio NO_DEFAULT_PATH)
+        PDA_LIBRARY pda
+        PATHS ${_PDA_PREFIX}/lib NO_DEFAULT_PATH)
+
+find_program(PDA_CONFIG
+        NAMES pda-config
+        PATHS ${_PDA_PREFIX}/bin NO_DEFAULT_PATH)
+
+if(PDA_CONFIG)
+execute_process(
+        COMMAND ${PDA_CONFIG} "--version"
+        OUTPUT_VARIABLE PDA_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PDA REQUIRED_VARS PDA_LIBRARY PDA_INCLUDE_DIR)
+find_package_handle_standard_args(PDA
+        REQUIRED_VARS PDA_LIBRARY PDA_INCLUDE_DIR
+        VERSION_VAR PDA_VERSION)
