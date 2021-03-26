190a61e9a948a3a798c693bfc62385131b237718
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2021-03-26 16:05:24 +0100

    Apply minor whitespace and parameter description changes

--- app/flesnet/main.cpp
+++ app/flesnet/main.cpp
@@ -24,7 +24,6 @@ volatile sig_atomic_t signal_status = 0;
 static void signal_handler(int sig) { signal_status = sig; }
 
 int main(int argc, char* argv[]) {
-
   std::signal(SIGINT, signal_handler);
   std::signal(SIGTERM, signal_handler);
 
