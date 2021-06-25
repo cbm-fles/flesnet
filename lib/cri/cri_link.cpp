159696c08ec90fe08134c1623a155edc55cf5c4b
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2021-06-25 20:45:24 +0200

    Adapted software to reworked cri hardware monitoring. - bumped cri version

--- lib/cri/cri_link.cpp
+++ lib/cri/cri_link.cpp
@@ -23,10 +23,6 @@ cri_link::cri_link(size_t link_index, pda::device* dev, pda::pci_bar* bar)
       std::unique_ptr<register_file>(new register_file_bar(bar, m_base_addr));
   m_rfgtx = std::unique_ptr<register_file>(
       new register_file_bar(bar, (m_base_addr + (1 << CRI_C_DMA_ADDR_SEL))));
-  // initialize cached members
-  m_reg_perf_interval_cached = m_rfpkt->get_reg(CRI_REG_PERF_INTERVAL);
-  m_reg_gtx_perf_interval_cached = 1; // TODO
-  //  m_rfgtx->get_reg(CRI_REG_GTX_PERF_INTERVAL);
 }
 
 cri_link::~cri_link() { deinit_dma(); }
@@ -92,7 +88,8 @@ std::ostream& operator<<(std::ostream& os, cri::cri_link::data_source_t src) {
     os << "   pgen";
     break;
   default:
-    os.setstate(std::ios_base::failbit);
+    // os.setstate(std::ios_base::failbit); will stall output
+    os << "invalid";
   }
   return os;
 }
@@ -131,84 +128,61 @@ uint32_t cri_link::get_pgen_mc_pending() {
 
 //////*** Performance Counters ***//////
 
-// set messurement avaraging interval in ms (max 17s)
-void cri_link::set_perf_interval(uint32_t interval) {
-  if (interval > 17000) {
-    interval = 17000;
-  }
-  // pkt domain
-  m_reg_perf_interval_cached = interval * (pkt_clk * 1E-3);
-  m_rfpkt->set_reg(CRI_REG_PERF_INTERVAL, m_reg_perf_interval_cached);
-  // gtx domain
-  // m_reg_gtx_perf_interval_cached = interval * (gtx_clk * 1E-3);
-  // m_rfgtx->set_reg(CRI_REG_GTX_PERF_INTERVAL,
-  // m_reg_gtx_perf_interval_cached);
+// synchonously capture and/or reset all perf counters
+void cri_link::set_perf_cnt(bool capture, bool reset) {
+  uint32_t reg = 0;
+  reg |= ((capture << 1) | reset);
+  m_rfpkt->set_reg(CRI_REG_PKT_PERF_CFG, reg, 0x3);
 }
 
-uint32_t cri_link::get_perf_interval_cycles_pkt() {
-  return m_reg_perf_interval_cached;
+// get perf interval in clock cycles
+uint32_t cri_link::get_perf_cycles() {
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_CYCLE);
 }
 
-// uint32_t cri_link::get_perf_interval_cycles_gtx() {
-//   return m_reg_gtx_perf_interval_cached;
-// }
+// words accepted by the dma mux (cycles)
+uint32_t cri_link::get_dma_trans() {
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_DMA_TRANS);
+}
 
-// packetizer could not send data (pkt cycles)
+// word not accepted due to back pressure from dma mux (cycles)
 uint32_t cri_link::get_dma_stall() {
-  return m_rfpkt->get_reg(CRI_REG_PERF_DMA_STALL);
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_DMA_STALL);
 }
 
-// packatizer stall from data buffer pointer match (pkt cycles)
+// back pressure from dma mux which DMA idle (cycles)
+uint32_t cri_link::get_dma_busy() {
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_DMA_BUSY);
+}
+
+// packatizer stall from data buffer pointer match (cycles)
 uint32_t cri_link::get_data_buf_stall() {
-  return m_rfpkt->get_reg(CRI_REG_PERF_EBUF_STALL);
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_EBUF_STALL);
 }
 
-// packatizer stall from descriptor buffer pointer match (pkt cycles)
+// packatizer stall from descriptor buffer pointer match (cycles)
 uint32_t cri_link::get_desc_buf_stall() {
-  return m_rfpkt->get_reg(CRI_REG_PERF_RBUF_STALL);
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_RBUF_STALL);
 }
 
-// number of events (ref. pkt)
-uint32_t cri_link::get_event_cnt() {
-  return m_rfpkt->get_reg(CRI_REG_PERF_N_EVENTS);
+// number of microslices (ref. pkt)
+uint32_t cri_link::get_microslice_cnt() {
+  return m_rfpkt->get_reg(CRI_REG_PKT_PERF_N_EVENTS);
 }
 
-// event rate in Hz
-float cri_link::get_event_rate() {
-  float n_events = static_cast<float>(m_rfpkt->get_reg(CRI_REG_PERF_N_EVENTS));
-  return n_events / (static_cast<float>(m_reg_perf_interval_cached) / pkt_clk);
-}
-
-// backpressure from packetizer input fifo (gtx cycles)
-uint32_t cri_link::get_din_full_gtx() {
-  //  return m_rfgtx->get_reg(CRI_REG_GTX_PERF_PKT_AFULL);
-  return 0; // TODO fake
-}
-
-cri_link::link_perf_t cri_link::link_perf() {
-  link_perf_t perf;
-  perf.pkt_cycle_cnt = m_reg_perf_interval_cached;
+cri_link::ch_perf_t cri_link::get_perf() {
+  ch_perf_t perf;
+  // capture and rest perf counters
+  set_perf_cnt(true, true);
+  // return shadowed counters
+  perf.cycles = get_perf_cycles();
+  perf.dma_trans = get_dma_trans();
   perf.dma_stall = get_dma_stall();
+  perf.dma_busy = get_dma_busy();
   perf.data_buf_stall = get_data_buf_stall();
   perf.desc_buf_stall = get_desc_buf_stall();
-  perf.events = get_event_cnt();
-  perf.gtx_cycle_cnt = m_reg_gtx_perf_interval_cached;
-  perf.din_full_gtx = get_din_full_gtx();
+  perf.microslice_cnt = get_microslice_cnt();
   return perf;
-}
-
-std::string cri_link::print_perf_raw() {
-  std::stringstream ss;
-  ss << "pkt_interval " << m_rfpkt->get_reg(CRI_REG_PERF_INTERVAL)
-     << "\n"
-     //     << "gtx_interval " << m_rfgtx->get_reg(CRI_REG_GTX_PERF_INTERVAL) <<
-     //     "\n"
-     << "event rate " << m_rfpkt->get_reg(CRI_REG_PERF_N_EVENTS) << "\n"
-     << "dma stall " << m_rfpkt->get_reg(CRI_REG_PERF_DMA_STALL) << "\n"
-     << "data buf stall " << m_rfpkt->get_reg(CRI_REG_PERF_EBUF_STALL) << "\n"
-     << "desc buf stall " << m_rfpkt->get_reg(CRI_REG_PERF_RBUF_STALL) << "\n";
-  //     << "din full " << m_rfgtx->get_reg(CRI_REG_GTX_PERF_PKT_AFULL) << "\n";
-  return ss.str();
 }
 
 } // namespace cri
