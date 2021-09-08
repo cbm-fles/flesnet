c8c65fb12d15e5c71d6e51e775dfbd55067d69be
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2021-09-08 23:40:29 +0200

    Add thread names to ease debugging

--- lib/fles_core/ConnectionGroupWorker.hpp
+++ lib/fles_core/ConnectionGroupWorker.hpp
@@ -12,5 +12,14 @@ class ConnectionGroupWorker : public ThreadContainer {
 public:
   /// The "main" function of an IBConnectionGroup & ConnectionGroup decendant.
   virtual void operator()() = 0;
+
+  /**
+   * @brief Return a text description of the object (to be used as a thread
+   * name).
+   *
+   * @return A string describing the object (at most 15 characters long).
+   */
+  [[nodiscard]] virtual std::string thread_name() const { return "CGWorker"; };
+
   virtual ~ConnectionGroupWorker() = default;
 };
