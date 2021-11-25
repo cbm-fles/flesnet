d45e8eabc894d2da1d035b10e8e46cb9a5acc4df
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2021-11-25 17:39:10 +0100

    Catch and discard ZMQ EINTR exception

--- lib/shm_ipc/ItemProducer.hpp
+++ lib/shm_ipc/ItemProducer.hpp
@@ -27,10 +27,18 @@ public:
 
   bool try_receive_completion(ItemID* id) {
     zmq::message_t message;
-    const auto result =
-        distributor_socket_.recv(message, zmq::recv_flags::dontwait);
-    if (!result.has_value()) {
-      return false;
+    try {
+      const auto result =
+          distributor_socket_.recv(message, zmq::recv_flags::dontwait);
+      if (!result.has_value()) {
+        return false;
+      }
+    } catch (zmq::error_t& ex) {
+      if (ex.num() == EINTR) {
+        return false;
+      } else {
+        throw;
+      }
     }
     *id = std::stoull(message.to_string());
     return true;
