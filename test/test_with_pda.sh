60b074eb9d40af820201763c583fd1c67a6611a3
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2022-06-17 02:07:21 +0200

    Fix the pda test

--- test/test_with_pda.sh
+++ test/test_with_pda.sh
@@ -3,12 +3,12 @@
 # test if build includes executables depending on libpda.
 
 if lsmod | grep -q uio_pci_dma; then
-    # try to discover FLIBs
+    # try to discover CRIs
     echo "Testing with pda kernel module"
-    ./flib_info
+    ./cri_info
 else
     # try to start an executable only available if build includes libpda
     # without actually using anything from libpda.
     echo "Testing without pda kernel module"
-    ./flib_server -h
+    ./cri_server -h
 fi    
