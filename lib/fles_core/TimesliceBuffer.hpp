9108def2406a1f9fb2ff1d5acd5eb7b8f2414e8e
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2023-09-12 16:57:10 +0200

    Improve TimesliceBuffer documentation

--- lib/fles_core/TimesliceBuffer.hpp
+++ lib/fles_core/TimesliceBuffer.hpp
@@ -40,39 +40,54 @@ public:
   /// The TimesliceBuffer destructor.
   ~TimesliceBuffer();
 
+  /// Get the 2's exponent of the size of the data buffer per input node in
+  /// bytes.
   [[nodiscard]] uint32_t get_data_size_exp() const {
     return data_buffer_size_exp_;
   }
+
+  /// Get the 2's exponent of the size of the descriptor buffer per input node
+  /// in units of TimesliceComponentDescriptors.
   [[nodiscard]] uint32_t get_desc_size_exp() const {
     return desc_buffer_size_exp_;
   }
 
-  uint8_t* get_data_ptr(uint_fast16_t index) {
+  /// Get the pointer to the data buffer of the specified input node.
+  [[nodiscard]] uint8_t* get_data_ptr(uint_fast16_t index) const {
     return data_ptr_ + index * (UINT64_C(1) << data_buffer_size_exp_);
   }
 
-  fles::TimesliceComponentDescriptor* get_desc_ptr(uint_fast16_t index) {
+  /// Get the pointer to the descriptor buffer of the specified input node.
+  [[nodiscard]] fles::TimesliceComponentDescriptor*
+  get_desc_ptr(uint_fast16_t index) {
     return desc_ptr_ + index * (UINT64_C(1) << desc_buffer_size_exp_);
   }
 
-  uint8_t& get_data(uint_fast16_t index, uint64_t offset) {
+  /// Get the reference to the data buffer of the specified input node at the
+  /// given offset.
+  [[nodiscard]] uint8_t& get_data(uint_fast16_t index, uint64_t offset) {
     offset &= (UINT64_C(1) << data_buffer_size_exp_) - 1;
     return get_data_ptr(index)[offset];
   }
 
-  fles::TimesliceComponentDescriptor& get_desc(uint_fast16_t index,
-                                               uint64_t offset) {
+  /// Get the reference to the descriptor buffer of the specified input node at
+  /// the given offset.
+  [[nodiscard]] fles::TimesliceComponentDescriptor&
+  get_desc(uint_fast16_t index, uint64_t offset) {
     offset &= (UINT64_C(1) << desc_buffer_size_exp_) - 1;
     return get_desc_ptr(index)[offset];
   }
 
+  /// Get the number of input nodes.
   [[nodiscard]] uint32_t get_num_input_nodes() const {
     return num_input_nodes_;
   }
 
+  /// Send a work item to the item distributor.
   void send_work_item(fles::TimesliceWorkItem wi);
 
-  bool try_receive_completion(fles::TimesliceCompletion& c) {
+  /// Receive a completion from the item distributor.
+  [[nodiscard]] bool try_receive_completion(fles::TimesliceCompletion& c) {
     ItemID id;
     if (!ItemProducer::try_receive_completion(&id)) {
       return false;
@@ -86,14 +101,15 @@ public:
 
   // Remaining member functions are for backwards compatibility only
 
-  void send_completion(fles::TimesliceCompletion /* c */) {
+  [[deprecated]] void send_completion(fles::TimesliceCompletion /* c */) {
     throw std::runtime_error(
         "TimesliceBuffer::send_completion() not supported");
   }
 
-  void send_end_work_item() {}
+  // Deprecated. Do not use.
+  [[deprecated]] void send_end_work_item() {}
 
-  void send_end_completion() {}
+  [[deprecated]] void send_end_completion() {}
 
   [[nodiscard]] std::size_t get_num_work_items() const {
     return outstanding_.size();
@@ -104,14 +120,18 @@ public:
   [[nodiscard]] std::string description() const;
 
 private:
-  std::string shm_identifier_;
-  boost::uuids::uuid shm_uuid_{};
-  uint32_t data_buffer_size_exp_;
-  uint32_t desc_buffer_size_exp_;
-  uint32_t num_input_nodes_;
+  std::string shm_identifier_;    ///< shared memory identifier
+  boost::uuids::uuid shm_uuid_{}; ///< shared memory UUID
+  uint32_t data_buffer_size_exp_; ///< 2's exponent of data buffer size in bytes
+  uint32_t desc_buffer_size_exp_; ///< 2's exponent of descriptor buffer size
+                                  ///< in units of TimesliceComponentDescriptors
+  uint32_t num_input_nodes_;      // number of input nodes
 
-  std::unique_ptr<boost::interprocess::managed_shared_memory> managed_shm_;
-  uint8_t* data_ptr_;
-  fles::TimesliceComponentDescriptor* desc_ptr_;
-  std::set<ItemID> outstanding_;
+  std::unique_ptr<boost::interprocess::managed_shared_memory>
+      managed_shm_;   ///< shared memory object
+  uint8_t* data_ptr_; ///< pointer to data buffer within shared memory
+  fles::TimesliceComponentDescriptor*
+      desc_ptr_;                 ///< pointer to descriptor
+                                 ///< buffer within shared memory
+  std::set<ItemID> outstanding_; ///< set of outstanding work items
 };
