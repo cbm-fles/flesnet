dc5d8602176f344bdde55e131590bb21105a24da
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2023-10-04 15:43:18 +0200

    Add missing ItemDistributor to ManagedTimesliceBuffer

--- lib/fles_core/ManagedTimesliceBuffer.cpp
+++ lib/fles_core/ManagedTimesliceBuffer.cpp
@@ -4,28 +4,35 @@
 #include "Timeslice.hpp"
 #include "TimesliceWorkItem.hpp"
 #include <chrono>
-#include <thread>
 
 ManagedTimesliceBuffer::ManagedTimesliceBuffer(
     zmq::context_t& context,
-    const std::string& distributor_address,
     const std::string& shm_identifier,
     uint32_t data_buffer_size_exp,
     uint32_t desc_buffer_size_exp,
     uint32_t num_input_nodes)
-    : timeslice_buffer_(context,
-                        distributor_address,
+    : producer_address_("inproc://" + shm_identifier),
+      worker_address_("ipc://@" + shm_identifier),
+      item_distributor_(context, producer_address_, worker_address_),
+      timeslice_buffer_(context,
+                        producer_address_,
                         shm_identifier,
                         data_buffer_size_exp,
                         desc_buffer_size_exp,
-                        num_input_nodes) {
-
-  for (uint_fast16_t i = 0; i < num_input_nodes; ++i) {
+                        num_input_nodes),
+      ack_(desc_buffer_size_exp),
+      distributor_thread_(std::ref(item_distributor_)) {
+  for (uint32_t i = 0; i < num_input_nodes; ++i) {
     desc_.emplace_back(timeslice_buffer_.get_desc_ptr(i),
                        timeslice_buffer_.get_desc_size_exp());
     data_.emplace_back(timeslice_buffer_.get_data_ptr(i),
                        timeslice_buffer_.get_data_size_exp());
   }
+}
+
+ManagedTimesliceBuffer::~ManagedTimesliceBuffer() {
+  item_distributor_.stop();
+  distributor_thread_.join();
 }
 
 void ManagedTimesliceBuffer::handle_timeslice_completions() {
@@ -35,7 +42,7 @@ void ManagedTimesliceBuffer::handle_timeslice_completions() {
       do {
         ++acked_;
       } while (ack_.at(acked_) > c.ts_pos);
-      for (uint_fast16_t i = 0; i < desc_.size(); ++i) {
+      for (std::size_t i = 0; i < desc_.size(); ++i) {
         desc_.at(i).set_read_index(acked_);
         data_.at(i).set_read_index(desc_.at(i).at(acked_ - 1).offset +
                                    desc_.at(i).at(acked_ - 1).size);
@@ -48,7 +55,7 @@ void ManagedTimesliceBuffer::handle_timeslice_completions() {
 
 bool ManagedTimesliceBuffer::timeslice_fits_in_buffer(
     const fles::Timeslice& timeslice) {
-  for (uint_fast16_t i = 0; i < timeslice.num_components(); ++i) {
+  for (uint64_t i = 0; i < timeslice.num_components(); ++i) {
     if (data_.at(i).size_available_contiguous() < timeslice.size_component(i) ||
         desc_.at(i).size_available() < 1) {
       return false;
@@ -65,7 +72,6 @@ void ManagedTimesliceBuffer::put(
   if (timeslice->num_components() != timeslice_buffer_.get_num_input_nodes()) {
     throw std::runtime_error("Timeslice has wrong number of components");
   }
-
   // Poll for timeslice completions until enough space is available.
   handle_timeslice_completions();
   while (!timeslice_fits_in_buffer(*timeslice)) {
@@ -74,7 +80,7 @@ void ManagedTimesliceBuffer::put(
   }
 
   // Copy each component to the shared memory buffer.
-  for (uint_fast16_t i = 0; i < timeslice->num_components(); ++i) {
+  for (uint64_t i = 0; i < timeslice->num_components(); ++i) {
 
     // Skip remaining bytes in the data buffer to avoid a fragmented entry.
     data_.at(i).skip_buffer_wrap(timeslice->size_component(i));
--- lib/fles_core/ManagedTimesliceBuffer.hpp
+++ lib/fles_core/ManagedTimesliceBuffer.hpp
@@ -1,11 +1,13 @@
 // Copyright 2023 Jan de Cuveland <cmail@cuveland.de>
 #pragma once
 
+#include "ItemDistributor.hpp"
 #include "ManagedRingBuffer.hpp"
 #include "RingBuffer.hpp"
 #include "Sink.hpp"
 #include "TimesliceBuffer.hpp"
 #include "TimesliceComponentDescriptor.hpp"
+#include <thread>
 #include <vector>
 #include <zmq.h>
 
@@ -18,11 +20,13 @@ class ManagedTimesliceBuffer : public fles::TimesliceSink {
 public:
   /// The ManagedTimesliceBuffer constructor.
   ManagedTimesliceBuffer(zmq::context_t& context,
-                         const std::string& distributor_address,
                          const std::string& shm_identifier,
                          uint32_t data_buffer_size_exp,
                          uint32_t desc_buffer_size_exp,
                          uint32_t num_input_nodes);
+
+  /// The ManagedTimesliceBuffer destructor.
+  ~ManagedTimesliceBuffer() override;
 
   ManagedTimesliceBuffer(const ManagedTimesliceBuffer&) = delete;
   void operator=(const ManagedTimesliceBuffer&) = delete;
@@ -30,8 +34,24 @@ public:
   void put(std::shared_ptr<const fles::Timeslice> timeslice) override;
 
 private:
+  /// Address that is used for communication between the TimesliceBuffer and the
+  /// ItemDistributor.
+  const std::string producer_address_;
+
+  /// Address that is used by Workers to connect to the ItemDistributor.
+  const std::string worker_address_;
+
+  /// The ItemDistributor object.
+  ItemDistributor item_distributor_;
+
   /// Shared memory buffer to store received timeslices.
   TimesliceBuffer timeslice_buffer_;
+
+  /// Buffer to store acknowledged status of timeslices.
+  RingBuffer<uint64_t, true> ack_;
+
+  /// Thread for the ItemDistributor.
+  std::thread distributor_thread_;
 
   /// The index of acknowledged timeslices (local buffer position).
   uint64_t acked_ = 0;
@@ -39,9 +59,6 @@ private:
   /// The index of the timeslice currently being received (local buffer
   /// position).
   uint64_t ts_pos_ = 0;
-
-  /// Buffer to store acknowledged status of timeslices.
-  RingBuffer<uint64_t, true> ack_;
 
   /// ManagedRingBuffer wrappers for the TimesliceComponentDescriptor buffer.
   std::vector<ManagedRingBuffer<fles::TimesliceComponentDescriptor>> desc_;
