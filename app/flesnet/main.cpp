75853424a188ed1fdb5c16504fb536f7a55db7a2
Benjamin Kaiser <kaiser@zib.de>
2024-02-27 17:00:58 +0100

    Includes documentation in .md files to doxygen

--- app/flesnet/main.cpp
+++ app/flesnet/main.cpp
@@ -1,17 +1,5 @@
 // Copyright 2012-2013 Jan de Cuveland <cmail@cuveland.de>
 
-/**
- * \mainpage InfiniBand-based Timeslice Building
- *
- * \section intro_sec Introduction
- *
- * The First-Level Event Selector (FLES) system of the CBM experiment
- * employs a scheme of timeslices (consisting of microslices) instead
- * of events in data aquisition. This software demonstrates the
- * timeslice building process between several nodes of the FLES
- * cluster over an Infiniband network.
- */
-
 #include "Application.hpp"
 #include "Parameters.hpp"
 #include "log.hpp"
