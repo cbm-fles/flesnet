481c99af6a55b157d0f0fcc9f72119d87677645f
Benjamin Kaiser <kaiser@zib.de>
2024-05-06 22:07:15 +0200

    Documents utils.hpp

--- app/tsa2msa/utils.hpp
+++ app/tsa2msa/utils.hpp
@@ -4,12 +4,38 @@
 #include <string>
 #include <vector>
 
+/**
+ * @file utils.hpp
+ * @brief Utility functions.
+ *
+ * \todo There is functionality in other files that could be moved here.
+ */
+
+/**
+ * @brief Compute the common prefix of a list of strings.
+ *
+ * @param strings The list of strings.
+ * @return The common prefix of the strings.
+ */
 std::string compute_common_prefix(const std::vector<std::string>& strings);
 
-// Remove everything except for the file name:
-//
-// TODO: Use boost::filesystem::path instead of the following hacky
-// code.
+/**
+ * @brief Clean up a path by removing everything except for the file name.
+ *
+ * @details This function is used to ensure that the output file names
+ * are placed in the current directory and not unexpectedly in the
+ * directory of the input files.
+ *
+ * \todo Use boost::filesystem::path instead of the following hacky
+ * solution.
+ *
+ * @note This function does not work if the path separator is not '/'.
+ * However, a compile time check is performed to ensure that the machine
+ * runs a linux. Possibly more portable solutions could be implemented.
+ *
+ * @param path The path to clean up.
+ *
+ */
 void clean_up_path(std::string& path);
 
 #endif // UTILS_HPP
