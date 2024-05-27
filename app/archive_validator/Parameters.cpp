a6555872f27571fabecd4fc96c3aac92a7eea764
Nico Greve <greve@zib.de>
2024-05-27 09:39:01 +0200

    Cleanup
    - removed unsued function arguments
    - cleanup of binary arguments and fixing typos

--- app/archive_validator/Parameters.cpp
+++ app/archive_validator/Parameters.cpp
@@ -34,30 +34,40 @@ void Parameters::parse_options(int argc, char* argv[]) {
 
   po::options_description archive_validation("Archive validation options");
   auto validation_add = archive_validation.add_options();
-  validation_add("timeslice-size", po::value<uint64_t>(&timeslice_size),
-              "Used for archive validation. Set the exepected timeslice size.");
   validation_add("timeslice-cnt", po::value<uint64_t>(&timeslice_cnt),
-              "Used for archive validation. Amount of expected timeslices in the timeslice archive.");
+              "Amount of expected timeslices in the timeslice archive.");
   validation_add("overlap", po::value<uint64_t>(&overlap)->default_value(overlap),
-              "Used for archive validation. Timeslice overlap size.");
-  validation_add("input-archives", po::value<std::vector<std::string>>(&input_archives_)->multitoken(),
-            "paths to the input archives to use for validation");
-  validation_add("output-archives", po::value<std::vector<std::string>>(&output_archives_)->multitoken(),
-            "paths to the output archives to use for validation");
+              "Timeslice overlap size.");
+  validation_add("input-archives", po::value<std::vector<std::string>>(&input_archives)->multitoken(),
+            "Paths to the input microslice archives.");
+  validation_add("output-archives", po::value<std::vector<std::string>>(&output_archives)->multitoken(),
+            "Paths to the output timeslice archives.");
   po::options_description desc;
-  desc.add(archive_validation);
+  desc
+    .add(general)
+    .add(archive_validation);
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
   po::notify(vm);
 
   if (vm.count("help") != 0u) {
-    std::cout << "mstool, git revision " << g_GIT_REVISION << std::endl;
+    std::cout << "archive_validator, git revision " << g_GIT_REVISION << std::endl;
     std::cout << desc << std::endl;
     exit(EXIT_SUCCESS);
   }
 
-  validate_ = vm.count("input-archives") + vm.count("output-archives") > 0;
+  logging::add_console(static_cast<severity_level>(log_level));
+  if (vm.count("log-file") != 0u) {
+    L_(info) << "Logging output to " << log_file;
+    logging::add_file(log_file, static_cast<severity_level>(log_level));
+  }
+  if (vm.count("log-syslog") != 0u) {
+    logging::add_syslog(logging::syslog::local0,
+                        static_cast<severity_level>(log_syslog));
+  }
+
+  validate = vm.count("input-archives") + vm.count("output-archives") > 0;
   
   bool analyze_parameter_set = true;
   if (vm.count("timeslice-cnt") == 0) {
@@ -65,11 +75,6 @@ void Parameters::parse_options(int argc, char* argv[]) {
     analyze_parameter_set = false;
   }
   
-  if (vm.count("timeslice-size") == 0) {
-    L_(fatal) << "'timeslice-size' option not set";
-    analyze_parameter_set = false;
-  }
-
   if (!analyze_parameter_set) {
     throw ParametersException("Not all necessary parameter for archive analyzing set.");
   }
