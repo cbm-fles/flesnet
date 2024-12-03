84ae55cdd6d51cd19f4998eefb143b9a44155efc
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2024-12-03 17:23:50 +0100

    Upgrade PDA version

--- contrib/pda_inst.sh
+++ contrib/pda_inst.sh
@@ -3,14 +3,14 @@
 set -e
 set -u
 
-PDA_VERSION="11.7.7"
-PDA_KERNEL_VERSION="0.10.0"
+PDA_VERSION="11.8.7"
+PDA_KERNEL_VERSION="0.11.0"
 USER_NAME=`id -u -n`
 
 install_kernel()
 {
   cd /tmp
-  rm pda-kernel-dkms_$2-1_amd64.deb
+  rm -f pda-kernel-dkms_$2-1_amd64.deb
   wget https://github.com/cbm-fles/pda/releases/download/$1/pda-kernel-dkms_$2-1_amd64.deb
   apt-get install ./pda-kernel-dkms_$2-1_amd64.deb
 }
@@ -18,7 +18,7 @@ install_kernel()
 install_lib()
 {
   cd /tmp
-  rm libpda4_$1-1_amd64.deb
+  rm -f libpda4_$1-1_amd64.deb
   wget https://github.com/cbm-fles/pda/releases/download/$1/libpda4_$1-1_amd64.deb
   apt-get install ./libpda4_$1-1_amd64.deb
 }
